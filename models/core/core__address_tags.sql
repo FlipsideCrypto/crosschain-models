fe8a2ffe5996d92df4ab11c7bb1e074c5b281c0b
gronders <grahamonders@gmail.com>
2022-07-22 10:25:06 -0600

    addded gnosis safe to core

--- models/core/core__address_tags.sql
+++ models/core/core__address_tags.sql
@@ -14,3 +14,15 @@ select
     _inserted_timestamp
 from 
     {{ref('silver__tags_contract_address')}}
+union
+select 
+    blockchain,
+    creator,
+    address,
+    tag_name,
+    tag_type,
+    start_date,
+    end_date,
+    _inserted_timestamp
+from 
+    {{ref('silver__tags_gnosis_safe_address')}}
