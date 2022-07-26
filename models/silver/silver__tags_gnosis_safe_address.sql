7b9531cc82295e7aa25b029a9f6bdc90c82c3968
gronders <grahamonders@gmail.com>
2022-07-26 09:37:26 -0600

    fixed logs table reference

--- models/silver/silver__tags_gnosis_safe_address.sql
+++ models/silver/silver__tags_gnosis_safe_address.sql
@@ -7,7 +7,7 @@
 SELECT
     DISTINCT 'ethereum' AS blockchain,
     'flipside' AS creator,
-    TX_JSON:receipt:logs[0]:decoded:inputs:instantiation::string AS address,
+    event_inputs:instantiation::string AS address,
     'gnosis safe address' AS tag_name,
     'contract' AS tag_type,
     DATE_TRUNC(
@@ -17,8 +17,8 @@ SELECT
     NULL AS end_date,
     current_timestamp as _inserted_timestamp
 FROM
-    {{source('ethereum_core', 'fact_transactions')}}
-WHERE TX_JSON:receipt:logs[0]:decoded:inputs:instantiation != 'null'
+    {{source('ethereum_silver', 'logs')}}
+WHERE event_name = 'ContractInstantiation'
     
     {% if is_incremental() %} 
     and 
