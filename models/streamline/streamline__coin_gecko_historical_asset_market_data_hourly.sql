97bcc87f29f1f705f7cc8a9a07cbb6d965bb615f
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-08-10 08:59:17 -0700

    views for streamline coingecko ohlc and historical (#12)

---
+++ models/streamline/streamline__coin_gecko_historical_asset_market_data_hourly.sql
@@ -0,0 +1,34 @@
+{{ config(
+    materialized = 'view',
+) }}
+
+SELECT
+    id,
+    90 AS days
+FROM
+    {{ source(
+        'crosschain_external',
+        'asset_metadata_api'
+    ) }}
+WHERE
+    provider = 'coingecko'
+    AND _inserted_date = (
+        SELECT
+            MAX(_inserted_date)
+        FROM
+            {{ source(
+                'crosschain_external',
+                'asset_metadata_api'
+            ) }}
+        WHERE
+            provider = 'coingecko'
+    )
+EXCEPT
+SELECT
+    id,
+    90
+FROM
+    {{ source(
+        'crosschain_external',
+        'asset_historical_hourly_market_data_coingecko_api'
+    ) }}
