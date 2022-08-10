ffa94484673f636831e6f6dda02a85a07c98506d
Graham Onders <grahamonders@gmail.com>
2022-08-10 10:28:02 -0600

    Tags and NTR table (#7)
    * added thorchain tags for dex and lp
    
    * fixed timestamp
    
    * active on ETH and L2 last 7 full history
    
    * sources
    
    * uncomment
    
    * added ntr. Fixed active last 7
    
    * fix dates table
    
    * removed where tag, having tx >1, and some order by
    
    * fixed active last 7 code!

---
+++ models/silver/silver__tags_thor_dex_user.sql
@@ -0,0 +1,142 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "address",
+    incremental_strategy = 'delete+insert',
+) }}
+
+WITH from_addresses AS (
+
+    SELECT
+        DISTINCT CASE
+            blockchain
+            WHEN 'BNB' THEN 'bsc'
+            WHEN 'GAIA' THEN 'cosmos'
+            WHEN 'ETH' THEN 'ethereum'
+            WHEN 'TERRA' THEN 'terra'
+            WHEN 'BCH' THEN 'bitcoin cash'
+            WHEN 'THOR' THEN 'thorchain'
+            WHEN 'LTC' THEN 'litecoin'
+            WHEN 'DOGE' THEN 'dogechain'
+            WHEN 'BTC' THEN 'bitcoin'
+        END AS blockchain,
+        'flipside' AS creator,
+        from_address AS address,
+        'thorchain dex user' AS tag_name,
+        'dex' AS tag_type,
+        MIN(block_id) as block_id,
+        MIN(DATE_TRUNC('day', block_timestamp)) AS start_date,
+        NULL AS end_date,
+        CURRENT_TIMESTAMP AS tag_created_at
+    FROM
+         {{source('thorchain', 'swaps')}}
+
+    {% if is_incremental() %}
+    WHERE
+    block_id not in (
+        SELECT
+            distinct block_id
+        FROM
+            {{ this }}
+    )
+    {% endif %}
+    GROUP BY
+    blockchain,
+        address
+),
+to_addresses AS (
+    SELECT
+        DISTINCT CASE
+            WHEN LEFT(
+                native_to_address,
+                3
+            ) = 'bnb' THEN 'bsc'
+            WHEN LEFT(
+                native_to_address,
+                6
+            ) = 'cosmos' THEN 'cosmos'
+            WHEN LEFT(
+                native_to_address,
+                2
+            ) = '0x' THEN 'ethereum'
+            WHEN LEFT(
+                native_to_address,
+                5
+            ) = 'terra' THEN 'terra'
+            WHEN LEFT(
+                native_to_address,
+                1
+            ) = 'q' THEN 'bitcoin cash'
+            WHEN LEFT(
+                native_to_address,
+                4
+            ) = 'thor' THEN 'thorchain'
+            WHEN LEFT(
+                native_to_address,
+                3
+            ) = 'ltc' THEN 'litecoin'
+            WHEN LEFT(
+                native_to_address,
+                1
+            ) = 'M' THEN 'litecoin'
+            WHEN LEFT(
+                native_to_address,
+                1
+            ) = 'L' THEN 'litecoin'
+            WHEN LEFT(
+                native_to_address,
+                1
+            ) = 'D' THEN 'dogechain'
+            WHEN LEFT(
+                native_to_address,
+                2
+            ) = 'bc' THEN 'bitcoin'
+            WHEN LEFT(
+                native_to_address,
+                1
+            ) = '1' THEN 'bitcoin'
+            WHEN LEFT(
+                native_to_address,
+                1
+            ) = '3' THEN 'bitcoin'
+            ELSE 'error'
+        END AS blockchain,
+        'flipside' AS creator,
+        native_to_address AS address,
+        'thorchain dex user' AS tag_name,
+        'dex' AS tag_type,
+        MIN(block_id) as block_id,
+        MIN(DATE_TRUNC('day', block_timestamp)) AS start_date,
+        NULL AS end_date,
+        current_timestamp as tag_created_at
+    FROM
+        {{source('thorchain', 'swaps')}}
+    WHERE
+        blockchain != 'error'
+        AND native_to_address NOT IN (
+            SELECT
+                DISTINCT address
+            FROM
+                from_addresses
+        )
+
+        {% if is_incremental() %}
+        and
+        block_id not in (
+            SELECT
+                distinct block_id
+            FROM
+                {{ this }}
+        )
+        {% endif %}
+    GROUP BY
+        address
+)
+SELECT
+    *
+FROM
+    from_addresses
+UNION
+SELECT
+    *
+FROM
+    to_addresses
