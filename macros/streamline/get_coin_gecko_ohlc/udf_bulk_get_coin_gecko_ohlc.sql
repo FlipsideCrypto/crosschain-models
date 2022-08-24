eeed833eefb6a23da79289d36edfc20d3bc060a0
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-08-24 14:31:40 -0700

    An 1871/switch cmc ohlc job to tasks (#20)
    * udf endpoint condition on database name
    
    * schedule cmc ohlc job with sf task
    
    * also refresh asset table in case of new/deprecated coins

--- macros/streamline/get_coin_gecko_ohlc/udf_bulk_get_coin_gecko_ohlc.sql
+++ macros/streamline/get_coin_gecko_ohlc/udf_bulk_get_coin_gecko_ohlc.sql
@@ -1,6 +1,6 @@
 {% macro udf_bulk_get_coin_gecko_ohlc() %}
     CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_coin_gecko_ohlc() returns text api_integration = aws_crosschain_api_dev AS {% if target.name == "prod" -%}
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_coin_gecko_ohlc() returns text api_integration = aws_crosschain_api_dev AS {% if target.database == "CROSSCHAIN" -%}
         'https://q2il6n5mmg.execute-api.us-east-1.amazonaws.com/prod/bulk_get_coin_gecko_ohlc'
     {% else %}
         'https://ubuxgfotp2.execute-api.us-east-1.amazonaws.com/dev/bulk_get_coin_gecko_ohlc'
