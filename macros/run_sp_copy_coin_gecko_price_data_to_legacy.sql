2f6662251a07a010c431a8d6bdfae2e9fc92dc9b
Desmond Hui <desmond@flipsidecrypto.com>
2022-09-07 08:18:08 -0700

    make diff call for dev

--- macros/run_sp_copy_coin_gecko_price_data_to_legacy.sql
+++ macros/run_sp_copy_coin_gecko_price_data_to_legacy.sql
@@ -1,7 +1,13 @@
 {% macro run_sp_copy_coin_gecko_price_data_to_legacy() %}
-{% set sql %}
-call crosschain.silver.copy_coin_gecko_price_data_to_legacy('flipside_prod_db');
-{% endset %}
+{% if target.database == 'CROSSCHAIN' %}
+    {% set sql %}
+    call silver.copy_coin_gecko_price_data_to_legacy('flipside_prod_db');
+    {% endset %}
+{% else %}
+    {% set sql %}
+    call silver.copy_coin_gecko_price_data_to_legacy('flipside_dev_db');
+    {% endset %}
+{% endif %}
 
 {% do run_query(sql) %}
 {% endmacro %}
