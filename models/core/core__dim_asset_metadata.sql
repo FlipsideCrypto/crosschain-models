101a99a94b3273b562d108e613ce8755cf35e29f
Desmond Hui <desmond@flipsidecrypto.com>
2022-09-13 11:34:52 -0700

    add cmc metadata

--- models/core/core__dim_asset_metadata.sql
+++ models/core/core__dim_asset_metadata.sql
@@ -7,7 +7,17 @@ SELECT
     id,
     token_address,
     NAME,
-    symbol,
+    upper(symbol) as symbol,
     platform
 FROM
     {{ ref('silver__asset_metadata_coin_gecko') }}
+UNION 
+SELECT
+    'coinmarketcap' AS provider,
+    id,
+    token_address,
+    NAME,
+    upper(symbol) as symbol,
+    platform
+FROM
+    {{ ref('silver__asset_metadata_coin_market_cap') }}
