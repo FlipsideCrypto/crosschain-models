003283063e73840f7ccfd1bccf5df1eeed5a1b8d
Desmond Hui <desmond@flipsidecrypto.com>
2022-09-14 09:49:18 -0700

    add prices model for cmc

---
+++ models/bronze/bronze__hourly_prices_coin_market_cap.sql
@@ -0,0 +1,12 @@
+{{ config(
+    materialized = 'view',
+) }}
+
+SELECT
+    *,
+    TO_TIMESTAMP_NTZ(SUBSTR(SPLIT_PART(metadata$filename, '/', 5), 1, 10) :: NUMBER, 0) AS _inserted_timestamp
+FROM
+    {{ source(
+        'crosschain_external',
+        'asset_ohlc_coin_market_cap_api'
+    ) }}
--- models/core/core__fact_hourly_prices.sql
+++ models/core/core__fact_hourly_prices.sql
@@ -14,3 +14,16 @@ SELECT
 FROM
     {{ ref('silver__hourly_prices_coin_gecko') }}
     p
+UNION
+SELECT
+    'coinmarketcap' AS provider,
+    p.id::string as id,
+    p.recorded_hour,
+    p.open,
+    p.high,
+    p.low,
+    p.close
+FROM
+    {{ ref('silver__hourly_prices_coin_market_cap') }}
+    p
+
