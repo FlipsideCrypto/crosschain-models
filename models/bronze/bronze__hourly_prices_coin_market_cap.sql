b906f15f0e647161f8a23cbb9abd3bd511f6c3ae
Desmond Hui <desmond@flipsidecrypto.com>
2022-09-19 10:42:33 -0700

    change model references to new streamline sources location

--- models/bronze/bronze__hourly_prices_coin_market_cap.sql
+++ models/bronze/bronze__hourly_prices_coin_market_cap.sql
@@ -7,6 +7,6 @@ SELECT
     TO_TIMESTAMP_NTZ(SUBSTR(SPLIT_PART(metadata$filename, '/', 5), 1, 10) :: NUMBER, 0) AS _inserted_timestamp
 FROM
     {{ source(
-        'crosschain_external',
+        'bronze_streamline',
         'asset_ohlc_coin_market_cap_api'
     ) }}
