5ad75fe0ad9e7520c0588fb53ce5af5c7e1686a0
gronders <grahamonders@gmail.com>
2022-10-13 16:18:22 -0600

    removed algorand

--- models/silver/labels/silver_crosschain__address_labels.sql
+++ models/silver/labels/silver_crosschain__address_labels.sql
@@ -234,21 +234,6 @@ FROM
 
 -- deposit wallet algos
 
--- UNION ALL 
-
--- SELECT 
---     system_created_at, 
---     insert_date, 
---     blockchain, 
---     address, 
---     creator, 
---     l1_label AS label_type, 
---     l2_label AS label_subtype, 
---     address_name, 
---     project_name, 
---     NULL AS delete_flag
--- FROM 
---     {{ ref('silver_crosschain__snowflake_Algorand_satellite') }}
 
 UNION ALL 
 
