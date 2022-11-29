6a2dc054143142042bb65ad06bd186066c15c389
Desmond Hui <desmond@flipsidecrypto.com>
2022-11-29 12:54:30 -0800

    switch task to use xsmall warehouse

--- macros/streamline/get_coin_market_cap_prices/task_run_sp_bulk_get_coin_market_cap_prices.sql
+++ macros/streamline/get_coin_market_cap_prices/task_run_sp_bulk_get_coin_market_cap_prices.sql
@@ -1,6 +1,6 @@
 {% macro task_run_sp_bulk_get_coin_market_cap_prices(resume_or_suspend) -%}
     create or replace task silver.run_sp_bulk_get_coin_market_cap_prices
-        warehouse = dbt_cloud
+        warehouse = dbt_cloud_legacy_prices
         schedule = 'USING CRON * * * * * UTC'
     as
         call silver.sp_bulk_get_coin_market_cap_prices();
