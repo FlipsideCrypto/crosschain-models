54dd848a90e75c8574a79ab787f0018de7374628
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2023-01-17 10:08:20 -0800

    remove legacy and unused resources (#89)

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,11 +1,8 @@
 {% macro create_udfs() %}
     {% set sql %}
         CREATE schema if NOT EXISTS streamline;
-        {{ udf_bulk_fill_cmc_historical_price_gaps() }};
         {{ udf_bulk_get_coin_gecko_asset_metadata() }};
         {{ udf_bulk_get_coin_market_cap_asset_metadata() }};
-        {{ udf_bulk_get_coin_market_cap_prices() }};
-        {{ udf_bulk_get_coin_gecko_prices() }};
         {{ udf_bulk_get_coin_market_cap_hourly_ohlc() }};
         {{ udf_bulk_get_coin_gecko_ohlc() }};
         {{ udf_bulk_get_coin_gecko_asset_market_data_historical() }};
