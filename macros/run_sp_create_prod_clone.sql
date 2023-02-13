1ee61411a6ff1c92f367c241d8cc6a29954da5a0
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2023-02-13 10:34:12 -0800

    Adjust for role changes (#103)
    * execute as caller
    
    * change dev db ownership

--- macros/run_sp_create_prod_clone.sql
+++ macros/run_sp_create_prod_clone.sql
@@ -3,7 +3,7 @@
     call crosschain._internal.create_prod_clone(
         'crosschain',
         'crosschain_dev',
-        'internal_dev'
+        'crosschain_dev_owner'
     );
 {% endset %}
     {% do run_query(clone_query) %}
