7f7b9d2fdf98da49e04149898b3edf1d4847d49c
Desmond Hui <desmond@flipsidecrypto.com>
2023-02-20 10:27:16 -0800

    update with prod endpoint

--- macros/streamline/get_coin_gecko_asset_metadata/udf_bulk_get_coin_gecko_asset_metadata.sql
+++ macros/streamline/get_coin_gecko_asset_metadata/udf_bulk_get_coin_gecko_asset_metadata.sql
@@ -1,8 +1,8 @@
 {% macro udf_bulk_get_coin_gecko_asset_metadata() %}
     CREATE
     OR REPLACE EXTERNAL FUNCTION silver.udf_bulk_get_coin_gecko_asset_metadata() returns text api_integration = aws_crosschain_api_dev AS {% if target.database == "CROSSCHAIN" -%}
-        'https://q2il6n5mmg.execute-api.us-east-1.amazonaws.com/prod/bulk_get_coin_gecko_asset_metadata'
+        'https://35hm1qhag9.execute-api.us-east-1.amazonaws.com/prod/bulk_get_coin_gecko_asset_metadata'
     {% else %}
-        'https://ubuxgfotp2.execute-api.us-east-1.amazonaws.com/dev/bulk_get_coin_gecko_asset_metadata'
+        'https://tlbh2d47i2.execute-api.us-east-1.amazonaws.com/dev/bulk_get_coin_gecko_asset_metadata'
     {%- endif %}
 {% endmacro %}
--- macros/streamline/get_coin_gecko_ohlc/udf_bulk_get_coin_gecko_ohlc.sql
+++ macros/streamline/get_coin_gecko_ohlc/udf_bulk_get_coin_gecko_ohlc.sql
@@ -1,7 +1,7 @@
 {% macro udf_bulk_get_coin_gecko_ohlc() %}
     CREATE
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_coin_gecko_ohlc() returns text api_integration = aws_crosschain_api_dev AS {% if target.database == "CROSSCHAIN" -%}
-        'https://q2il6n5mmg.execute-api.us-east-1.amazonaws.com/prod/bulk_get_coin_gecko_hourly_ohlc'
+        'https://35hm1qhag9.execute-api.us-east-1.amazonaws.com/prod/bulk_get_coin_gecko_hourly_ohlc'
     {% else %}
         'https://tlbh2d47i2.execute-api.us-east-1.amazonaws.com/dev/bulk_get_coin_gecko_hourly_ohlc'
     {%- endif %}
