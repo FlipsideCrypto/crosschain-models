3f731dbaee15144683cde64c6be4b69c33d514a1
drethereum <trevor.wenokur@gmail.com>
2023-04-05 13:50:49 -0600

    moved asset_metadata contracts join to view layer

--- models/core/core__dim_asset_metadata.sql
+++ models/core/core__dim_asset_metadata.sql
@@ -9,12 +9,11 @@ SELECT
     id,
     COALESCE(c.symbol,s.symbol) AS symbol,
     name,
-    c.decimals AS decimals,
-    s.blockchain AS blockchain,
+    decimals,
+    s.blockchain,
     provider
 FROM {{ ref('silver__asset_metadata_all_providers') }} s
 LEFT JOIN {{ ref('core__dim_contracts') }} c 
-    ON LOWER(c.address) = s.token_address 
-        AND c.blockchain = s.blockchain
-QUALIFY(ROW_NUMBER() OVER (PARTITION BY token_address, id, COALESCE(c.symbol,s.symbol), s.blockchain
+    ON LOWER(c.address) = s.token_address AND c.blockchain = s.blockchain
+QUALIFY(ROW_NUMBER() OVER (PARTITION BY token_address, id, COALESCE(c.symbol,s.symbol), s.blockchain 
     ORDER BY provider)) = 1
