71c129568f698364e729890b775718f32add9a81
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-04-12 10:37:55 -0400

    add cosmos tokens

--- models/core/core__ez_asset_metadata.sql
+++ models/core/core__ez_asset_metadata.sql
@@ -6,30 +6,40 @@
 
 WITH base_priority AS (
 
-SELECT
-    token_address,
-    id,
-    COALESCE(c.symbol,s.symbol) AS symbol,
-    name,
-    decimals,
-    s.blockchain,
-    provider,
-    CASE
-        WHEN provider = 'coingecko' THEN 1
-        WHEN provider = 'coinmarketcap' THEN 2
-    END AS priority
-FROM {{ ref('silver__asset_metadata_priority') }} s
-LEFT JOIN {{ ref('core__dim_contracts') }} c 
-    ON LOWER(c.address) = s.token_address AND c.blockchain = s.blockchain
+    SELECT
+        token_address,
+        id,
+        COALESCE(
+            C.symbol,
+            s.symbol
+        ) AS symbol,
+        NAME,
+        decimals,
+        s.blockchain,
+        provider,
+        CASE
+            WHEN provider = 'coingecko' THEN 1
+            WHEN provider = 'coinmarketcap' THEN 2
+        END AS priority
+    FROM
+        {{ ref('silver__asset_metadata_priority') }}
+        s
+        LEFT JOIN {{ ref('core__dim_contracts') }} C
+        ON LOWER(
+            C.address
+        ) = LOWER(
+            s.token_address
+        )
+        AND C.blockchain = s.blockchain
 )
-
 SELECT
     token_address,
     id,
     symbol,
-    name,
+    NAME,
     decimals,
     blockchain
-FROM base_priority
-QUALIFY(ROW_NUMBER() OVER (PARTITION BY token_address, blockchain
-    ORDER BY priority ASC)) = 1
+FROM
+    base_priority qualify(ROW_NUMBER() over (PARTITION BY token_address, blockchain
+ORDER BY
+    priority ASC)) = 1
--- models/silver/hourly_prices/silver__token_prices_priority_hourly.sql
+++ models/silver/hourly_prices/silver__token_prices_priority_hourly.sql
@@ -7,53 +7,66 @@
 ) }}
 
 WITH all_providers AS (
-    
-SELECT
-    hour,
-    token_address,
-    blockchain,
-    provider,
-    price,
-    is_imputed,
-    _inserted_timestamp
-FROM {{ ref('silver__token_prices_all_providers_hourly') }}
-{% if is_incremental() %}
-WHERE _inserted_timestamp > (
+
     SELECT
-        MAX(_inserted_timestamp)
+        HOUR,
+        token_address,
+        blockchain,
+        provider,
+        price,
+        is_imputed,
+        _inserted_timestamp
     FROM
-        {{ this }}
+        {{ ref('silver__token_prices_all_providers_hourly') }}
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp > (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
     )
 {% endif %}
 ),
-
 all_prices AS (
-
-SELECT 
-    hour,
-    token_address,
-    blockchain,
-    provider,
-    price,
-    is_imputed,
-    _inserted_timestamp,
-    CASE
-        WHEN provider = 'coingecko' AND is_imputed = FALSE THEN 1
-        WHEN provider = 'coinmarketcap' AND is_imputed = FALSE THEN 2
-        WHEN provider = 'coingecko' AND is_imputed = TRUE THEN 3
-        WHEN provider = 'coinmarketcap' AND is_imputed = TRUE THEN 4
-    END AS priority
-FROM all_providers
+    SELECT
+        HOUR,
+        token_address,
+        blockchain,
+        provider,
+        price,
+        is_imputed,
+        _inserted_timestamp,
+        CASE
+            WHEN provider = 'coingecko'
+            AND is_imputed = FALSE THEN 1
+            WHEN provider = 'coinmarketcap'
+            AND is_imputed = FALSE THEN 2
+            WHEN provider = 'coingecko'
+            AND is_imputed = TRUE THEN 3
+            WHEN provider = 'coinmarketcap'
+            AND is_imputed = TRUE THEN 4
+            WHEN provider = 'pool balance' THEN 5
+            WHEN provider = 'swap' THEN 6
+        END AS priority
+    FROM
+        all_providers
 )
-
 SELECT
-    hour,
-    LOWER(token_address) AS token_address,
+    HOUR,
+    CASE
+        WHEN token_address ILIKE 'ibc%' THEN token_address
+        ELSE LOWER(token_address)
+    END AS token_address,
     price,
     blockchain,
     is_imputed,
     _inserted_timestamp,
-    {{ dbt_utils.surrogate_key( ['hour', 'token_address', 'blockchain'] ) }} AS _unique_key
-FROM all_prices
-QUALIFY(ROW_NUMBER() OVER (PARTITION BY hour, token_address, blockchain 
-    ORDER BY priority ASC)) = 1
+    {{ dbt_utils.surrogate_key(
+        ['hour', 'token_address', 'blockchain']
+    ) }} AS _unique_key
+FROM
+    all_prices qualify(ROW_NUMBER() over (PARTITION BY HOUR, token_address, blockchain
+ORDER BY
+    priority ASC)) = 1
