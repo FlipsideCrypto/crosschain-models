6f06be86f5a50f6899e0820d7e954366020005ad
gronders <grahamonders@gmail.com>
2023-06-06 10:04:22 -0600

    add to core table

--- models/silver/tags/Ethereum/silver__tags_token_standard.sql
+++ models/silver/tags/Ethereum/silver__tags_token_standard.sql
@@ -4,38 +4,36 @@
     incremental_strategy = 'delete+insert',
 ) }}
 
-WITH base AS (
-
-    SELECT
-        DISTINCT 'ethereum' AS blockchain,
-        'flipside' AS creator,
-        contract_address AS address,
-        CASE
-            WHEN topics [0] = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62' THEN 'erc-1155'
-            WHEN (
-                topics [0] = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-                AND topics [3] IS NOT NULL
-            ) THEN 'erc-721'
-            ELSE 'erc-20'
-        END AS tag_name,
-        'token standard' AS tag_type,
-        MIN(
-            block_timestamp :: DATE
-        ) AS start_date,
-        NULL AS end_date,
-        CURRENT_TIMESTAMP AS tag_created_at,
-        MIN(_inserted_timestamp) AS _inserted_timestamp
-    FROM
-         {{ source(
-            'ethereum_silver',
-            'decoded_logs'
-        ) }}
-    WHERE
-        topics [0] IN (
-            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-            '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
-        )
-        AND decoded_flat :from = '0x0000000000000000000000000000000000000000'
+SELECT
+    DISTINCT 'ethereum' AS blockchain,
+    'flipside' AS creator,
+    contract_address AS address,
+    CASE
+        WHEN topics [0] = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62' THEN 'erc-1155'
+        WHEN (
+            topics [0] = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+            AND topics [3] IS NOT NULL
+        ) THEN 'erc-721'
+        ELSE 'erc-20'
+    END AS tag_name,
+    'token standard' AS tag_type,
+    MIN(
+        block_timestamp :: DATE
+    ) AS start_date,
+    NULL AS end_date,
+    CURRENT_TIMESTAMP AS tag_created_at,
+    MIN(_inserted_timestamp) AS _inserted_timestamp
+FROM
+    {{ source(
+        'ethereum_silver',
+        'decoded_logs'
+    ) }}
+WHERE
+    topics [0] IN (
+        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
+    )
+    AND decoded_flat :from = '0x0000000000000000000000000000000000000000'
 
 {% if is_incremental() %}
 AND _INSERTED_TIMESTAMP > (
@@ -44,7 +42,12 @@ AND _INSERTED_TIMESTAMP > (
     FROM
         {{ this }}
 )
-and contract_address not in (select distinct address from {{this}})
+AND contract_address NOT IN (
+    SELECT
+        DISTINCT address
+    FROM
+        {{ this }}
+)
 {% endif %}
 GROUP BY
     1,
@@ -52,26 +55,3 @@ GROUP BY
     3,
     4,
     5
-),
-double_tag AS (
-    SELECT
-        DISTINCT address,
-        COUNT(tag_name) AS counts
-    FROM
-        base
-    GROUP BY
-        1
-    HAVING
-        counts > 1
-)
-SELECT
-    DISTINCT *
-FROM
-    base
-WHERE
-    address NOT IN (
-        SELECT
-            DISTINCT address
-        FROM
-            double_tag
-    )
