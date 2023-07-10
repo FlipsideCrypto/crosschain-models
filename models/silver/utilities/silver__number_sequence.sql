c629468c2b8d203048ebdc64b09776de27147e19
Austin <austin@flipsidecrypto.com>
2023-07-10 11:27:15 -0400

    add sequence

---
+++ models/silver/utilities/silver__number_sequence.sql
@@ -0,0 +1,13 @@
+{{ config(
+    materialized = 'table',
+    cluster_by = 'round(_id,-5)',
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
+) }}
+
+SELECT
+    ROW_NUMBER() over (
+        ORDER BY
+            SEQ4()
+    ) :: INT AS _id
+FROM
+    TABLE(GENERATOR(rowcount => 1000000000))
