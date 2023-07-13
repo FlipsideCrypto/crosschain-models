d70a1231d864e95d6f752d788948c80a2fccad54
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-07-12 23:22:01 -0400

    add new models (#152)

--- models/silver/observability/silver__data_observability_results.sql
+++ models/silver/observability/silver__data_observability_results.sql
@@ -2,17 +2,37 @@
     materialized = 'view'
 ) }}
 
-{% set models = [ 
-('avalanche', source('avalanche_observ', 'blocks_completeness')), 
-('avalanche', source('avalanche_observ', 'logs_completeness')), 
-('avalanche', source('avalanche_observ', 'receipts_completeness')), 
-('avalanche', source('avalanche_observ', 'traces_completeness')), 
-('avalanche', source('avalanche_observ', 'transactions_completeness')), 
-('cosmos', source('cosmos_observ', 'blocks_completeness')), 
-('cosmos', source('cosmos_observ', 'transactions_completeness')), 
-('osmosis', source('osmosis_observ', 'blocks_completeness')),
-('osmosis', source('osmosis_observ', 'transactions_completeness'))
-] 
+{% set models = [
+    ('avalanche', source('avalanche_observ', 'blocks_completeness')),
+    ('avalanche', source('avalanche_observ', 'logs_completeness')),
+    ('avalanche', source('avalanche_observ', 'receipts_completeness')),
+    ('avalanche', source('avalanche_observ', 'traces_completeness')),
+    ('avalanche', source('avalanche_observ', 'transactions_completeness')),
+    ('ethereum', source('ethereum_observ', 'blocks_completeness')),
+    ('ethereum', source('ethereum_observ', 'logs_completeness')),
+    ('ethereum', source('ethereum_observ', 'receipts_completeness')),
+    ('ethereum', source('ethereum_observ', 'traces_completeness')),
+    ('ethereum', source('ethereum_observ', 'transactions_completeness')),
+    ('arbitrum', source('arbitrum_observ', 'blocks_completeness')),
+    ('arbitrum', source('arbitrum_observ', 'logs_completeness')),
+    ('arbitrum', source('arbitrum_observ', 'receipts_completeness')),
+    ('arbitrum', source('arbitrum_observ', 'traces_completeness')),
+    ('arbitrum', source('arbitrum_observ', 'transactions_completeness')),
+    ('bsc', source('bsc_observ', 'blocks_completeness')),
+    ('bsc', source('bsc_observ', 'logs_completeness')),
+    ('bsc', source('bsc_observ', 'receipts_completeness')),
+    ('bsc', source('bsc_observ', 'traces_completeness')),
+    ('bsc', source('bsc_observ', 'transactions_completeness')),
+    ('polygon', source('polygon_observ', 'blocks_completeness')),
+    ('polygon', source('polygon_observ', 'logs_completeness')),
+    ('polygon', source('polygon_observ', 'receipts_completeness')),
+    ('polygon', source('polygon_observ', 'traces_completeness')),
+    ('polygon', source('polygon_observ', 'transactions_completeness')),
+    ('cosmos', source('cosmos_observ', 'blocks_completeness')),
+    ('cosmos', source('cosmos_observ', 'transactions_completeness')),
+    ('osmosis', source('osmosis_observ', 'blocks_completeness')),
+    ('osmosis', source('osmosis_observ', 'transactions_completeness'))
+]
 %}
 SELECT *
 FROM (
