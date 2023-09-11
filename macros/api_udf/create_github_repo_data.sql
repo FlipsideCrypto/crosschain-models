4e898a0cf6d03bdec8da4255283ceb73862b76b2
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-09-11 14:45:56 +0200

    fix: arrays

--- macros/api_udf/create_github_repo_data.sql
+++ macros/api_udf/create_github_repo_data.sql
@@ -1,8 +1,8 @@
 {% macro create_github_repo_data() %}
 {% set create_table %}
-CREATE schema if NOT EXISTS {{ target.database }}.bronze_api;
+CREATE SCHEMA IF NOT EXISTS {{ target.database }}.bronze_api;
 
-CREATE TABLE if NOT EXISTS {{ target.database }}.bronze_api.github_repo_data(
+CREATE TABLE IF NOT EXISTS {{ target.database }}.bronze_api.github_repo_data(
     repo_name STRING,
     endpoint_name STRING,
     data VARIANT,
@@ -12,11 +12,11 @@ CREATE TABLE if NOT EXISTS {{ target.database }}.bronze_api.github_repo_data(
 {% endset %}
 {% do run_query(create_table) %}
 {% set query %}
-CREATE OR REPLACE PROCEDURE {{ target.database }}.bronze_api.get_github_repo_data() returns VARIANT LANGUAGE SQL AS $$
+CREATE OR REPLACE PROCEDURE {{ target.database }}.bronze_api.get_github_repo_data() RETURNS VARIANT LANGUAGE SQL AS $$
 BEGIN
-    -- Replace with the list of repositories you are interested in
-    let repos = ARRAY['owner/repo1', 'owner/repo2', ...];  
-    let endpoints = ARRAY[
+    let base_url := 'https://api.github.com';
+    let repos := ARRAY_CONSTRUCT['CADigitalNexus/Wikis'];
+    let endpoints := ARRAY_CONSTRUCT[
         '/repos/{owner}/{repo}/community/profile',
         '/repos/{owner}/{repo}/stats/code_frequency',
         '/repos/{owner}/{repo}/stats/commit_activity',
@@ -31,32 +31,46 @@ BEGIN
         '/repos/{owner}/{repo}/releases',
         '/repos/{owner}/{repo}/forks'
     ];
-    let base_url = 'https://api.github.com';
+    let counter:= 0;
+    let repo_counter:= 0;
+    let endpoint_counter:= 0;
+    let total_repos := ARRAY_SIZE(repos);
+    let total_endpoints := ARRAY_SIZE(endpoints);
 
-    FOR repo in repos
-    BEGIN
-        FOR endpoint in endpoints
-        BEGIN
-            -- Replace {owner} and {repo} in endpoint with actual values
-            let endpoint_url = base_url || REPLACE(REPLACE(endpoint, '{owner}', SPLIT_PART(repo, '/', 1)), '{repo}', SPLIT_PART(repo, '/', 2));
-            let response = livequery.live.udf_api('GET', endpoint_url, { "Authorization": "Bearer YOUR_GITHUB_TOKEN" });  -- Replace YOUR_GITHUB_TOKEN with your actual token
-            -- Insert the result into the bronze_api.github_repo_data table
-            INSERT INTO {{ target.database }}.bronze_api.github_repo_data(
-                repo_name,
-                endpoint_name,
-                data,
-                _inserted_timestamp,
-                _res_id
-            )
-            VALUES (
-                repo,
-                endpoint,
-                response,
-                CURRENT_TIMESTAMP,
-                repo || '-' || endpoint
-            );
-        END;
-    END;
+    REPEAT
+        -- Extract current repo and endpoint
+        let current_repo = repos[repo_counter + 1];
+        let current_endpoint = endpoints[endpoint_counter + 1];
+        let endpoint_url = base_url || REPLACE(REPLACE(current_endpoint, '{owner}', SPLIT_PART(current_repo, '/', 1)), '{repo}', SPLIT_PART(current_repo, '/', 2));
+        let response = livequery.live.udf_api('GET', endpoint_url, { "Authorization": "Bearer YOUR_GITHUB_TOKEN" });
+
+        -- Insert the result into the bronze_api.github_repo_data table
+        INSERT INTO {{ target.database }}.bronze_api.github_repo_data(
+            repo_name,
+            endpoint_name,
+            data,
+            _inserted_timestamp,
+            _res_id
+        )
+        VALUES (
+            current_repo,
+            current_endpoint,
+            response,
+            CURRENT_TIMESTAMP,
+            current_repo || '-' || current_endpoint
+        );
+
+        -- Update counters
+        endpoint_counter:= endpoint_counter + 1;
+        IF endpoint_counter = total_endpoints THEN
+            endpoint_counter := 0;
+            repo_counter:= repo_counter + 1;
+        END IF;
+        counter:= counter + 1;
+
+    UNTIL repo_counter = total_repos
+    END REPEAT;
+
     RETURN 'Success';
 END;$$ 
 {% endset %}
