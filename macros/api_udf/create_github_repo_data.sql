a55ee6b92c4ebfbd800247d89d67055a8964b460
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-09-11 18:28:57 +0200

    working version

--- macros/api_udf/create_github_repo_data.sql
+++ macros/api_udf/create_github_repo_data.sql
@@ -1,7 +1,7 @@
 {% macro create_github_repo_data() %}
   {% set create_table %}
   CREATE SCHEMA IF NOT EXISTS {{ target.database }}.bronze_api;
-  CREATE TABLE IF NOT EXISTS {{ target.database }}.bronze_api.github_repo_data(
+  CREATE OR REPLACE TABLE {{ target.database }}.bronze_api.github_repo_data(
       repo_owner STRING,
       repo_name STRING,
       endpoint_name STRING,
@@ -14,29 +14,35 @@
   {% do run_query(create_table) %}
   
   {% set query %}
-  CREATE OR REPLACE PROCEDURE {{ target.database }}.bronze_api.get_github_repo_data() RETURNS VARIANT LANGUAGE javascript AS $$
+  CREATE OR REPLACE PROCEDURE {{ target.database }}.bronze_api.get_github_repo_data() RETURNS VARIANT LANGUAGE javascript EXECUTE AS CALLER AS $$
+
+    function logMessage(message) {
+       let logSql = `SELECT SYSTEM$LOG('${message}')`;
+       snowflake.execute({sqlText: logSql});
+    }
 
     let repos = ['CADigitalNexus/Wikis'];  // Replace with your repository specifications
     let endpoints = [
         '/repos/{owner}/{repo}/community/profile',
         '/repos/{owner}/{repo}/stats/code_frequency',
-        //'/repos/{owner}/{repo}/stats/commit_activity',
-        //'/repos/{owner}/{repo}/stats/contributors',
-        //'/repos/{owner}/{repo}/stats/participation',
-        //'/repos/{owner}/{repo}/stats/punch_card',
-        //'/repos/{owner}/{repo}/pulls',
-        //'/repos/{owner}/{repo}/issues',
-        //'/repos/{owner}/{repo}/stargazers',
-        //'/repos/{owner}/{repo}/subscribers',
-        //'/repos/{owner}/{repo}/commits',
-        //'/repos/{owner}/{repo}/releases',
-        //'/repos/{owner}/{repo}/forks'
+        '/repos/{owner}/{repo}/stats/commit_activity',
+        '/repos/{owner}/{repo}/stats/contributors',
+        '/repos/{owner}/{repo}/stats/participation',
+        '/repos/{owner}/{repo}/stats/punch_card',
+        '/repos/{owner}/{repo}/pulls',
+        '/repos/{owner}/{repo}/issues',
+        '/repos/{owner}/{repo}/stargazers',
+        '/repos/{owner}/{repo}/subscribers',
+        '/repos/{owner}/{repo}/commits',
+        '/repos/{owner}/{repo}/releases',
+        '/repos/{owner}/{repo}/forks'
     ];    
     let current_repo_full = '';
     let current_repo_owner = '';
     let current_repo_name = '';
     let current_endpoint = '';
     let endpoint_url = '';
+    let base_url = 'https://api.github.com'
 
     for (var repo_counter = 0; repo_counter < repos.length; repo_counter++) {
         current_repo_full = repos[repo_counter];
@@ -48,17 +54,17 @@
             current_endpoint = endpoints[endpoint_counter];
             let segments = current_endpoint.split('/');
             let lastSegment = segments[segments.length - 1];
-            endpoint_url = current_endpoint.replace('{owner}', current_repo_owner).replace('{repo}', current_repo_name);
+            endpoint_url = base_url + current_endpoint.replace('{owner}', current_repo_owner).replace('{repo}', current_repo_name);
             var create_temp_table_command = `
                 CREATE OR REPLACE TEMPORARY TABLE response_data AS 
                 WITH api_call AS (
                     SELECT 
-                        ethereum.streamline.udf_api('GET', '${endpoint_url}', { 'Authorization': 'Bearer YOUR_GITHUB_TOKEN' },{}) AS res,
+                        ethereum.streamline.udf_api('GET', '${endpoint_url}', { 'Authorization': 'token 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', 'Accept': 'application/vnd.github.v3+json' },{}) AS res,
                         CURRENT_TIMESTAMP AS _request_timestamp
                 ),
                 flatten_res AS (
                     SELECT 
-                        VALUE AS data,
+                        res AS data,
                         'github' AS provider,
                         _request_timestamp AS _inserted_timestamp,
                         concat_ws('-', DATE_PART(epoch_second, _request_timestamp), '${current_repo_name}', '${endpoint_url}') AS _res_id
@@ -87,7 +93,7 @@
                     _res_id
                 )
                 SELECT
-                    '${current_repo_owner}' as repo_owner,
+                    '${current_repo_owner}' as repo_owner,  
                     '${current_repo_name}' as repo_name,
                     '${lastSegment}' as endpoint_name,
                     data,
@@ -96,13 +102,10 @@
                     _res_id
                 FROM response_data;
             `;
-
-            snowflake.execute({sqlText: insert_command.replace('{current_repo_owner}', current_repo_owner).replace('{current_repo_name}', current_repo_name).replace('{lastSegment}', lastSegment)});
-
+            snowflake.execute({sqlText: insert_command});
 
         };
     };
-
     return 'Data fetched for all endpoints and repos';
   $$ 
   {% endset %}
