b95137ad6db4d16ac93dfc8ca7aaf97e894956ff
drethereum <trevor.wenokur@gmail.com>
2023-10-12 13:46:39 -0600

    syntax

--- models/gold/protocols/ens/ens__ez_ens_domains.sql
+++ models/gold/protocols/ens/ens__ez_ens_domains.sql
@@ -4,27 +4,7 @@
 ) }}
 
 SELECT
-    last_registered_block,
-    last_registered_timestamp,
-    last_registered_tx_hash,
-    last_registered_contract,
-    manager,
-    owner,
-    set_address,
-    ens_set,
-    ens_domain,
-    ens_subdomains,
-    label,
-    node,
-    token_id,
-    last_registered_cost,
-    last_registered_premium,
-    renewal_cost,
-    expiration_timestamp,
-    expired,
-    resolver,
-    profile,
-    last_updated
+    *
 FROM
     {{ source(
         'ethereum_ens',
