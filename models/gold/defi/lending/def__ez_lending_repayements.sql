b381cbca6fc220c24e86902d52d2f9098fe0babb
mattromano <mattromano88@gmail.com>
2023-10-24 13:11:05 -0700

    ethereum ez lending models

---
+++ models/gold/defi/lending/def__ez_lending_repayements.sql
@@ -0,0 +1,43 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true }
+) }}
+
+WITH base AS (
+
+    SELECT
+        'ethereum' AS blockchain,
+        platform,
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        protocol_token,
+        borrow_asset,
+        borrow_amount,
+        borrow_amount_usd,
+        borrow_symbol,
+        borrower_address,
+        borrow_rate_mode,
+        lending_pool_contract
+    FROM
+        {{ source(
+            'ethereum_defi',
+            'ez_lending_repayments'
+        ) }}
+)
+SELECT
+    blockchain,
+    platform,
+    block_number
+    block_timestamp
+    tx_hash
+    contract_address
+    pool_address,
+    pool_name,
+    tokens,
+    symbols,
+    decimals
+FROM
+    base
