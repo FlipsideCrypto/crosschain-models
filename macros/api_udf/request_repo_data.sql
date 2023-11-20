07d4b399baa286d11a89df45ee63896feeaaea34
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2023-11-20 14:56:14 -0800

    Rework near GitHub retry logic (#223)
    * update sp macro
    
    * reducing lambda
    
    * second debug
    
    * wait fix
    
    * fix batch
    
    * try just with 1
    
    * simplify batch results and retry logic
    
    * make response_data table temporary
    
    * remove unused var
    
    * fix syntax
    
    * remove union, limit 1000 endpoints per run
    
    * run every hour
    
    * add comment
    
    * do not execute if 0 rows or remaining calls less than needed for a batch
    
    ---------
    
    Co-authored-by: WHYTEWYLL <juanmiblanco1@gmail.com>

--- macros/api_udf/request_repo_data.sql
+++ macros/api_udf/request_repo_data.sql
@@ -39,35 +39,65 @@ CREATE OR REPLACE PROCEDURE {{ target.database }}.bronze_api.get_github_api_repo
         const MAX_BATCH_SIZE = 100;
 
         var row_count = 0;
-        var batch_num = 100;
-        var max_calls = 5000;
+        var batch_size = 100;
+        var max_batch_tries = 5;
+        var num_calls_remaining = 5000;
 
         var res = snowflake.execute({sqlText: `WITH subset as (
-                SELECT *
+                SELECT 
+                    *,
+                    row_number() over (order by full_endpoint) as rn, 
+                    floor(rn/${batch_size}) as gn
                 FROM {{ target.database }}.silver.github_repos
-                WHERE  (last_time_queried IS NULL OR DATE(last_time_queried) < DATEADD(DAY, -7, SYSDATE()))
+                WHERE (last_time_queried IS NULL OR DATE(last_time_queried) < DATEADD(DAY, -7, SYSDATE()))
                 AND frequency IN ${parsedFrequencyArray}
-                LIMIT 5000
             )
-            SELECT count(*)
-            FROM subset`});
-        res.next()
-        row_count = res.getColumnValue(1);
+            SELECT *
+            FROM subset
+            WHERE gn < 10`}); /* limit 1000 endpoints per run due to GH rate limits */
 
-        batch_num = Math.max(MIN_BATCH_SIZE, Math.min(MAX_BATCH_SIZE, Math.round(row_count * 0.05)));
+        var query_id = res.getQueryId();
 
-        call_groups = Math.round(max_calls/batch_num);
+        var get_max_batch_num_stmt = snowflake.execute({sqlText: `select coalesce(max(gn),-1) from table(result_scan('${query_id}'))`});
+        get_max_batch_num_stmt.next()
+        var max_batch_num = get_max_batch_num_stmt.getColumnValue(1);
 
-        for(let i = 0; i < call_groups; i++) {
+        for(let i = 0; i < max_batch_num+1; i++) {
+            var is_batch_done = false
+            var num_batch_tries = 0
 
-            var create_temp_table_command = `
-                CREATE OR REPLACE TEMPORARY TABLE {{ target.database }}.bronze_api.response_data AS
-                WITH api_call AS (
-            `;
+            /* need at least (batch_size * max_batch_tries) number of calls left in our hourly rate limit for this batch to work*/
+            if (num_calls_remaining < batch_size*max_batch_tries) {
+                snowflake.execute({sqlText: `INSERT INTO {{ target.database }}.bronze_api.log_messages (log_level, message) VALUES ('INFO', 'CALLS REMAINING,ending execution due to ${num_calls_remaining} calls remaining')`});
+                break;
+            }
 
-            for (let i = 0; i < batch_num; i++) {
-                create_temp_table_command += `
-                    SELECT * FROM (
+            /* seed the table for 1st run */
+            snowflake.execute({sqlText: `create or replace temporary table {{ target.database }}.bronze_api.response_data AS
+                select full_endpoint, NULL as status_code from table(result_scan('${query_id}'))`});
+
+            while(!is_batch_done) {
+                num_batch_tries = num_batch_tries + 1
+                /* log start */
+                snowflake.execute({sqlText: `INSERT INTO {{ target.database }}.bronze_api.log_messages (log_level, message) VALUES ('INFO', 'START,iteration ${num_batch_tries} of group ${i}')`});
+                var create_temp_table_command = `
+                    CREATE OR REPLACE TEMPORARY TABLE {{ target.database }}.bronze_api.response_data AS
+                    WITH subset AS (
+                        SELECT 
+                            project_name,
+                            SYSDATE() AS _request_timestamp,
+                            repo_url,
+                            full_endpoint,
+                            endpoint_github,
+                            row_number() over (order by full_endpoint) as subset_rn
+                        FROM 
+                            table(result_scan('${query_id}'))
+                        WHERE 
+                            gn = ${i}
+                        AND 
+                            full_endpoint in (SELECT full_endpoint FROM {{ target.database }}.bronze_api.response_data WHERE (status_code = 202 or status_code is NULL))
+                    ),
+                    api_call as (
                         SELECT
                             project_name,
                             {{ target.database }}.live.udf_api('GET', CONCAT('https://api.github.com', full_endpoint), { 'Authorization': CONCAT('token ', '{${TOKEN}}'), 'Accept': 'application/vnd.github+json'},{}, 'github_cred') AS res,
@@ -75,156 +105,97 @@ CREATE OR REPLACE PROCEDURE {{ target.database }}.bronze_api.get_github_api_repo
                             repo_url,
                             full_endpoint,
                             endpoint_github
-                        FROM {{ target.database }}.silver.github_repos
-                        WHERE     (last_time_queried IS NULL OR DATE(last_time_queried) < DATEADD(DAY, -7, SYSDATE()))
-                        AND frequency IN ${parsedFrequencyArray}
-                        ORDER BY full_endpoint, retries DESC
-                        LIMIT 1 OFFSET ${i}
+                        FROM 
+                            subset
+                    ),
+                    flatten_res AS (
+                        SELECT
+                            project_name, 
+                            repo_url,
+                            full_endpoint,
+                            res:data AS data,
+                            res:status_code AS status_code,
+                            'github' AS provider,
+                            endpoint_github,
+                            GET(res:headers, 'X-RateLimit-Remaining')::INTEGER as rate_limit_remaining,
+                            _request_timestamp AS _inserted_timestamp,
+                            CONCAT_WS('-', DATE_PART(epoch_second, _request_timestamp), SPLIT_PART(repo_url, '/', 2), full_endpoint) AS _res_id
+                        FROM api_call
                     )
-                `;
-
-                if (i < batch_num - 1) {
-                    create_temp_table_command += `  UNION ALL `;
-                }
-            }
-
-            create_temp_table_command += `
-                ),
-                flatten_res AS (
                     SELECT
-                        project_name, 
+                        project_name,
                         repo_url,
                         full_endpoint,
-                        res:data AS data,
-                        res:status_code AS status_code,
-                        'github' AS provider,
+                        data,
+                        status_code,
+                        provider,
                         endpoint_github,
-                        GET(res:headers, 'X-RateLimit-Remaining')::INTEGER as rate_limit_remaining,
-                        _request_timestamp AS _inserted_timestamp,
-                        CONCAT_WS('-', DATE_PART(epoch_second, _request_timestamp), SPLIT_PART(repo_url, '/', 2), full_endpoint) AS _res_id
-                    FROM api_call
-                )
-                SELECT
-                    project_name,
-                    repo_url,
-                    full_endpoint,
-                    data,
-                    status_code,
-                    provider,
-                    endpoint_github,
-                    rate_limit_remaining,
-                    _inserted_timestamp,
-                    _res_id
-                FROM
-                flatten_res;
-            `;
+                        rate_limit_remaining,
+                        _inserted_timestamp,
+                        _res_id
+                    FROM
+                    flatten_res;`;
 
-            snowflake.execute({sqlText: create_temp_table_command});
-            // Second command: Insert data into the target table from the temporary table
-            
-            var insert_command = `
-                INSERT INTO {{ target.database }}.bronze_api.github_repo_data(
-                            project_name,
-                            repo_owner,
-                            repo_name,
-                            endpoint_name,
-                            data,
-                            provider,
-                            endpoint_github,
-                            _inserted_timestamp,
-                            _res_id
-                        )
-                        SELECT
-                            project_name,
-                            SPLIT_PART(repo_url, '/', 1) as repo_owner,
-                            SPLIT_PART(repo_url, '/', 2) as repo_name,
-                            full_endpoint as endpoint_name,
-                            data,
-                            provider,
-                            endpoint_github,
-                            _inserted_timestamp,
-                            _res_id
-                        FROM {{ target.database }}.bronze_api.response_data
-                        WHERE rate_limit_remaining > 0 and status_code != 202;
-            `;
-            snowflake.execute({sqlText: insert_command});
-            // Update command: Update last_time_queried for the queried endpoints
-            var update_command = `
-                UPDATE {{ target.database }}.silver.github_repos
-                SET last_time_queried = SYSDATE(),
-                retries = 0
-                WHERE full_endpoint IN (SELECT full_endpoint FROM {{ target.database }}.bronze_api.response_data WHERE rate_limit_remaining > 0 and status_code != 202);
-            `;
-            snowflake.execute({sqlText: update_command});
+                snowflake.execute({sqlText: create_temp_table_command});
+                
+                var log_debug_status_code_message = `INSERT INTO {{ target.database }}.bronze_api.log_messages (log_level, message) select 'DEBUG', CONCAT_WS(',',full_endpoint,status_code) from {{ target.database }}.bronze_api.response_data`;
+                snowflake.execute({sqlText: log_debug_status_code_message});
 
-            var check_retries_command = `
-                SELECT COUNT(*) 
-                FROM {{ target.database }}.silver.github_repos 
-                WHERE retries > 5
-            `;
-            var result_set = snowflake.execute({sqlText: check_retries_command});
-            result_set.next();
-            var count_rows_with_excessive_retries = result_set.getColumnValue(1);
-
-
-
-            var update_retries_command = `
-                UPDATE {{ target.database }}.silver.github_repos
-                SET retries = retries + 1
-                WHERE full_endpoint IN (SELECT full_endpoint FROM {{ target.database }}.bronze_api.response_data WHERE status_code = 202);
-            `;
-            snowflake.execute({sqlText: update_retries_command});
-            
-            if (count_rows_with_excessive_retries > 0) {
-                var reset_retries_command = `
-                    UPDATE {{ target.database }}.silver.github_repos
-                    SET retries = 0
-                    WHERE retries > 5
+                var insert_command = `
+                    INSERT INTO {{ target.database }}.bronze_api.github_repo_data(
+                                project_name,
+                                repo_owner,
+                                repo_name,
+                                endpoint_name,
+                                data,
+                                provider,
+                                endpoint_github,
+                                _inserted_timestamp,
+                                _res_id
+                            )
+                            SELECT
+                                project_name,
+                                SPLIT_PART(repo_url, '/', 1) as repo_owner,
+                                SPLIT_PART(repo_url, '/', 2) as repo_name,
+                                full_endpoint as endpoint_name,
+                                data,
+                                provider,
+                                endpoint_github,
+                                _inserted_timestamp,
+                                _res_id
+                            FROM {{ target.database }}.bronze_api.response_data
+                            WHERE rate_limit_remaining > 0 and status_code != 202;
                 `;
-                snowflake.execute({sqlText: reset_retries_command});
-            }
+                snowflake.execute({sqlText: insert_command});
 
-            // Check if there are any rows with non-zero retries
-            var check_retries_query = `
-                SELECT COUNT(*) as count_rows
-                FROM {{ target.database }}.silver.github_repos
-                WHERE retries > 0
-            `;
-            var result_set_check = snowflake.execute({sqlText: check_retries_query});
-            result_set_check.next();
-            var count_rows_with_retries = result_set_check.getColumnValue(1);
+                var update_command = `
+                    UPDATE {{ target.database }}.silver.github_repos
+                    SET last_time_queried = SYSDATE(),
+                    retries = 0
+                    WHERE full_endpoint IN (SELECT full_endpoint FROM {{ target.database }}.bronze_api.response_data WHERE rate_limit_remaining > 0 and status_code != 202);
+                `;
+                snowflake.execute({sqlText: update_command});
 
-            if (count_rows_with_retries > 0) {
-                    // Fetch the retries value of the first row ordered by retries DESC
-                    var retries_query = `
-                        SELECT AVG(retries) as retries
-                        FROM {{ target.database }}.silver.github_repos
-                        ORDER BY retries DESC
-                        LIMIT ${batch_num}
-                    `;
-                    var result_set = snowflake.execute({sqlText: retries_query});
-                    result_set.next();
-                    var retries = result_set.getColumnValue(1);
+                var num_calls_remaining_res = snowflake.execute({sqlText: `SELECT min(rate_limit_remaining) FROM {{ target.database }}.bronze_api.response_data;`});
+                num_calls_remaining_res.next()
+                num_calls_remaining = num_calls_remaining_res.getColumnValue(1);
 
-                    // Determine the wait time based on the retries value
-                    // For example, if you want to wait 10 seconds for each retry:
-                    var wait_time = Math.round(2 * retries);
+                var need_to_retry_command = `SELECT full_endpoint FROM {{ target.database }}.bronze_api.response_data WHERE (status_code = 202 or status_code is NULL);`;
+                var need_to_retry_res = snowflake.execute({sqlText: need_to_retry_command});
+                var need_to_retry_row_count = need_to_retry_res.getRowCount();
+                var wait_seconds = 5
+                var wait_stmt = `CALL system$wait(${wait_seconds});`
 
-                    var wait_command = `
-                        CALL system$wait(${wait_time});
-                    `;
-
-
-                snowflake.execute({sqlText: wait_command});
-            }
-
-            else if (count_rows_with_retries = 0) {
-                    break;
+                if (need_to_retry_row_count == 0 || num_batch_tries >= 5) {
+                    is_batch_done = true
+                } else if (need_to_retry_row_count > 10) {
+                    snowflake.execute({sqlText: wait_stmt})
                 }
 
-            var log_message = `INSERT INTO {{ target.database }}.bronze_api.log_messages (log_level, message) VALUES ('INFO', ' Iteration ${i} of ${call_groups} complete.')`;
-            snowflake.execute({sqlText: log_message});
+                snowflake.execute({sqlText: `INSERT INTO {{ target.database }}.bronze_api.log_messages (log_level, message) VALUES ('INFO', 'CALLS REMAINING,${num_calls_remaining}')`});
+                snowflake.execute({sqlText: `INSERT INTO {{ target.database }}.bronze_api.log_messages (log_level, message) VALUES ('INFO', 'END,iteration ${num_batch_tries} of group ${i}')`});
             }
+        }
 
     return 'Success';
 
