8e55ddccc0239ea4793ec0df6a0ca8901345e9d0
shah <info@shahnewazkhan.ca>
2023-11-29 12:41:41 -0800

    removed unussed ohlc udf

--- macros/streamline/get_coin_gecko_ohlc/udf_bulk_get_coin_gecko_ohlc.sql
+++ macros/streamline/get_coin_gecko_ohlc/udf_bulk_get_coin_gecko_ohlc.sql
@@ -6,12 +6,3 @@
         'https://tlbh2d47i2.execute-api.us-east-1.amazonaws.com/dev/bulk_get_coin_gecko_hourly_ohlc'
     {%- endif %}
 {% endmacro %}
-
-{% macro udf_bulk_get_coin_gecko_historical_ohlc() %}
-    CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_coin_gecko_historical_ohlc() returns text api_integration = aws_crosschain_api_dev AS {% if target.database == "CROSSCHAIN" -%}
-        'https://35hm1qhag9.execute-api.us-east-1.amazonaws.com/prod/bulk_get_coin_gecko_historical_hourly_ohlc'
-    {% else %}
-        'https://tlbh2d47i2.execute-api.us-east-1.amazonaws.com/dev/bulk_get_coin_gecko_historical_hourly_ohlc'
-    {%- endif %}
-{% endmacro %}
