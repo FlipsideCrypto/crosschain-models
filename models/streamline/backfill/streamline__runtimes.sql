139d00cdc9c992e1fef9f9b89799a7c0ab694996
shah <info@shahnewazkhan.ca>
2023-11-30 09:10:43 -0800

    add dim_date ref()

--- models/streamline/backfill/streamline__runtimes.sql
+++ models/streamline/backfill/streamline__runtimes.sql
@@ -8,7 +8,7 @@ with base as (
     select
         date_day::TIMESTAMP as run_time
     from
-        crosschain_dev.core.dim_dates
+        {{ ref('core__dim_dates')}}
     where
         date_day between '2018-01-01' and dateadd(day, 4747, '2018-01-01')  -- coin gecko pro provides historical data from 2018-01-01, dim_dates has 4747 days for this range (2018-01-01 to 2030-12-31)
 )
