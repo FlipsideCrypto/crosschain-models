b3f41cad4cc65d66a9deb7c306105e13f41eb159
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-12-27 17:11:51 -0500

    stash (#233)
    * stash
    
    * 10 days

--- models/streamline/streamline__all_unknown_coin_gecko_asset_ohlc.sql
+++ models/streamline/streamline__all_unknown_coin_gecko_asset_ohlc.sql
@@ -2,14 +2,26 @@
     materialized = 'view',
 ) }}
 
+WITH dates AS (
+
+    SELECT
+        date_day :: timestamp_ltz AS run_time
+    FROM
+        {{ ref('core__dim_dates') }}
+    WHERE
+        date_day BETWEEN CURRENT_DATE - INTERVAL '10 day'
+        AND CURRENT_DATE
+)
 SELECT
     id,
-    date_trunc('hour',current_timestamp) AS run_time
+    run_time
 FROM
     {{ source(
         'bronze_streamline',
         'asset_metadata_coin_gecko_api'
     ) }}
+    JOIN dates
+    ON TRUE
 WHERE
     provider = 'coingecko'
     AND _inserted_date = (
@@ -26,7 +38,10 @@ WHERE
 EXCEPT
 SELECT
     id,
-    run_time
+    DATE_TRUNC(
+        'day',
+        run_time
+    ) :: timestamp_ltz AS run_time
 FROM
     {{ source(
         'bronze_streamline',
