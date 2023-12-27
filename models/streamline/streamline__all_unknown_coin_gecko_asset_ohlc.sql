a441f7d1710cf75db1d647dddfea38139c15cf2d
Austin <austin@flipsidecrypto.com>
2023-12-27 17:02:24 -0500

    stash

--- models/streamline/streamline__all_unknown_coin_gecko_asset_ohlc.sql
+++ models/streamline/streamline__all_unknown_coin_gecko_asset_ohlc.sql
@@ -2,14 +2,26 @@
     materialized = 'view',
 ) }}
 
+WITH dates AS (
+
+    SELECT
+        date_day :: timestamp_ltz AS run_time
+    FROM
+        {{ ref('core__dim_dates') }}
+    WHERE
+        date_day BETWEEN CURRENT_DATE - INTERVAL '7 day'
+        AND CURRENT_DATE
+)
 SELECT
     id,
-    date_trunc('hour',current_timestamp) AS run_time
+    run_time
 FROM
     {{ source(
         'bronze_streamline',
         'asset_metadata_coin_gecko_api'
     ) }}
+    JOIN dates
+    ON TRUE
 WHERE
     provider = 'coingecko'
     AND _inserted_date = (
