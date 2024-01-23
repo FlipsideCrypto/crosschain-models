1eb4cc67846497be9aa1fbd11527604754476c8f
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2024-01-23 15:26:56 -0500

    Deposit wallet intermed (#243)
    * staged deposit wallets
    
    * fix to incremental key
    
    * changed ref in gold to new table
    
    * fix to join
    
    * PR feedback changes
    
    * fale in dim table
    
    * create a silver dim_labels table with _is_deleted
    
    * name change
    
    * incremental logic
    
    * label source
    
    * remove creator
    
    * inc updates
    
    * test
    
    * keep pk the same
    
    * pk
    
    * id
    
    * id
    
    * format
    
    ---------
    
    Co-authored-by: gronders <grahamonders@gmail.com>

--- models/gold/core/core__dim_labels.sql
+++ models/gold/core/core__dim_labels.sql
@@ -9,62 +9,14 @@ SELECT
     blockchain,
     address,
     creator,
-    case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
+    label_type,
     label_subtype,
     address_name,
     project_name,
-    COALESCE(inserted_timestamp,'2000-01-01') as inserted_timestamp,
-    COALESCE(modified_timestamp,'2000-01-01') as modified_timestamp,
-    COALESCE(address_labels_id,{{ dbt_utils.generate_surrogate_key(['blockchain','creator','address']) }}) AS dim_labels_id
+    inserted_timestamp,
+    modified_timestamp,
+    labels_combined_id AS dim_labels_id
 FROM
-    {{ ref('silver__address_labels') }}
-where delete_flag is null 
-UNION ALL
-SELECT
-    system_created_at,
-    insert_date,
-    blockchain,
-    address,
-    creator,
-    case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
-    label_subtype,
-    address_name,
-    project_name,
-    COALESCE(inserted_timestamp,'2000-01-01') as inserted_timestamp,
-    COALESCE(modified_timestamp,'2000-01-01') as modified_timestamp,
-    COALESCE(deposit_wallets_id,{{ dbt_utils.generate_surrogate_key(['blockchain','creator','address']) }}) AS dim_labels_id
-FROM
-    {{ ref('silver__deposit_wallets') }}
-UNION ALL
-SELECT
-  system_created_at,
-  insert_date,
-  blockchain,
-  address,
-  creator,
-  case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
-  label_subtype,
-  address_name,
-  project_name,
-  COALESCE(inserted_timestamp,'2000-01-01') as inserted_timestamp,
-  COALESCE(modified_timestamp,'2000-01-01') as modified_timestamp,
-  COALESCE(contract_autolabels_id,{{ dbt_utils.generate_surrogate_key(['blockchain','creator','address']) }}) AS dim_labels_id
-FROM
-  {{ ref('silver__contract_autolabels') }}
-UNION ALL
-SELECT
-  system_created_at,
-  insert_date,
-  blockchain,
-  address,
-  creator,
-  case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
-  label_subtype,
-  address_name,
-  project_name,
-  COALESCE(inserted_timestamp,'2000-01-01') as inserted_timestamp,
-  COALESCE(modified_timestamp,'2000-01-01') as modified_timestamp,
-  COALESCE(labels_eth_contracts_table_id,{{ dbt_utils.generate_surrogate_key(['address']) }}) AS dim_labels_id
-FROM
-  {{ ref('silver__labels_eth_contracts_table') }}
-
+    {{ ref('silver__labels_combined') }}
+WHERE
+    _is_deleted = FALSE
