3f3672b4a3f767afddb45dbf54e0a9f43e952a4a
shah <info@shahnewazkhan.ca>
2024-01-27 14:00:38 -0800

    reduce udf invocation to 15 mins frequency

--- models/streamline/backfill/streamline__get_coingecko_prices_history.sql
+++ models/streamline/backfill/streamline__get_coingecko_prices_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_get_coin_gecko_prices(object_construct('sql_source', '{{this.identifier}}','external_table', 'ASSET_PRICES_API','producer_batch_size', '460000', 'worker_batch_size', '25000', 'sql_limit', '840000'))",
+        func = "{{this.schema}}.udf_bulk_get_coin_gecko_prices(object_construct('sql_source', '{{this.identifier}}','external_table', 'ASSET_PRICES_API','producer_batch_size', '640000', 'worker_batch_size', '25000', 'sql_limit', '1400000'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_prices_history']
