0d389e7ec6143d7f3470e085f814bbea204f1735
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-01-31 08:50:40 -0500

    rever formatting

--- models/silver/labels/silver__labels_combined.sql
+++ models/silver/labels/silver__labels_combined.sql
@@ -1,72 +1,31 @@
 {{ config(
-  materialized = 'incremental',
-  incremental_strategy = 'merge',
-  unique_key = ['blockchain','address'],
-  cluster_by = ['blockchain','_is_deleted','modified_timestamp::DATE'],
-  merge_exclude_columns = ["inserted_timestamp"],
-  post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(address)",
-  tags = ['snowflake', 'crosschain', 'labels', 'gold_address_labels']
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['blockchain','address'],
+    cluster_by = ['blockchain','_is_deleted','modified_timestamp::DATE'],    
+    merge_exclude_columns = ["inserted_timestamp"],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(address)",
+    tags = ['snowflake', 'crosschain', 'labels', 'gold_address_labels']
 ) }}
 
-WITH base AS (
-
-  SELECT
+WITH base as (
+SELECT
     system_created_at,
     insert_date,
     blockchain,
     address,
     creator,
-    CASE
-      WHEN label_type = 'layer2' THEN 'bridge'
-      ELSE label_type
-    END AS label_type,
+    case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
     label_subtype,
     address_name,
     project_name,
-    SYSDATE() AS inserted_timestamp,
-    SYSDATE() AS modified_timestamp,
+    SYSDATE() as inserted_timestamp,
+    SYSDATE() as modified_timestamp,
     {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }} AS labels_combined_id,
-    'address_labels' AS source,
-    CASE
-      WHEN delete_flag IS NULL THEN FALSE
-      ELSE TRUE
-    END AS _is_deleted
-  FROM
-    {{ ref('silver__address_labels') }}
-
-{% if is_incremental() %}
-WHERE
-  modified_timestamp >= (
-    SELECT
-      MAX(modified_timestamp)
-    FROM
-      {{ this }}
-    WHERE
-      source = 'address_labels'
-  )
-{% endif %}
-UNION ALL
-SELECT
-  system_created_at,
-  insert_date,
-  blockchain,
-  address,
-  creator,
-  CASE
-    WHEN label_type = 'layer2' THEN 'bridge'
-    ELSE label_type
-  END AS label_type,
-  label_subtype,
-  address_name,
-  project_name,
-  SYSDATE() AS inserted_timestamp,
-  SYSDATE() AS modified_timestamp,
-  {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }} AS labels_combined_id,
-  'deposit' AS source,
-  _is_deleted
+    'address_labels' as source,
+    case when delete_flag is null then FALSE else TRUE end as _is_deleted
 FROM
-  {{ ref('silver__deposit_wallets_full') }}
-
+    {{ ref('silver__address_labels') }}
 {% if is_incremental() %}
 WHERE
   modified_timestamp >= (
@@ -74,8 +33,35 @@ WHERE
       MAX(modified_timestamp)
     FROM
       {{ this }}
-    WHERE
-      source = 'deposit'
+    where source = 'address_labels'
+  )
+{% endif %}
+UNION ALL
+SELECT
+    system_created_at,
+    insert_date,
+    blockchain,
+    address,
+    creator,
+    case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
+    label_subtype,
+    address_name,
+    project_name,
+    SYSDATE() as inserted_timestamp,
+    SYSDATE() as modified_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }} AS labels_combined_id,
+    'deposit' as source,
+    _is_deleted
+FROM
+    {{ ref('silver__deposit_wallets_full') }}
+{% if is_incremental() %}
+WHERE
+  modified_timestamp >= (
+    SELECT
+      MAX(modified_timestamp)
+    FROM
+      {{ this }}
+    where source = 'deposit'
   )
 {% endif %}
 UNION ALL
@@ -85,21 +71,17 @@ SELECT
   blockchain,
   address,
   creator,
-  CASE
-    WHEN label_type = 'layer2' THEN 'bridge'
-    ELSE label_type
-  END AS label_type,
+  case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
   label_subtype,
   address_name,
   project_name,
-  SYSDATE() AS inserted_timestamp,
-  SYSDATE() AS modified_timestamp,
-  {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }} AS labels_combined_id,
-  'autolabel' AS source,
-  FALSE AS _is_deleted
+  SYSDATE() as inserted_timestamp,
+  SYSDATE() as modified_timestamp,
+  {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }}  AS labels_combined_id,
+  'autolabel' as source,
+  FALSE as _is_deleted
 FROM
   {{ ref('silver__contract_autolabels') }}
-
 {% if is_incremental() %}
 WHERE
   modified_timestamp >= (
@@ -107,8 +89,7 @@ WHERE
       MAX(modified_timestamp)
     FROM
       {{ this }}
-    WHERE
-      source = 'autolabel'
+    where source = 'autolabel'
   )
 {% endif %}
 UNION ALL
@@ -118,21 +99,17 @@ SELECT
   blockchain,
   address,
   creator,
-  CASE
-    WHEN label_type = 'layer2' THEN 'bridge'
-    ELSE label_type
-  END AS label_type,
+  case when label_type = 'layer2' then 'bridge' else label_type end as label_type,
   label_subtype,
   address_name,
   project_name,
-  SYSDATE() AS inserted_timestamp,
-  SYSDATE() AS modified_timestamp,
-  {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }} AS labels_combined_id,
-  'eth_contracts' AS source,
-  FALSE AS _is_deleted
+  SYSDATE() as inserted_timestamp,
+  SYSDATE() as modified_timestamp,
+  {{ dbt_utils.generate_surrogate_key(['blockchain','address']) }}  AS labels_combined_id,
+  'eth_contracts' as source,
+  FALSE as _is_deleted
 FROM
   {{ ref('silver__labels_eth_contracts_table') }}
-
 {% if is_incremental() %}
 WHERE
   modified_timestamp >= (
@@ -140,8 +117,7 @@ WHERE
       MAX(modified_timestamp)
     FROM
       {{ this }}
-    WHERE
-      source = 'eth_contracts'
+    where source = 'eth_contracts'
   )
 {% endif %}
 )
