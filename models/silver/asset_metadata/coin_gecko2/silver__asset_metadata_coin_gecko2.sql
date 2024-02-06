78b73fe57ea369dba53c5875660b1cf95ae1774f
drethereum <trevor.wenokur@gmail.com>
2024-02-06 13:12:57 -0700

    draft

---
+++ models/silver/asset_metadata/coin_gecko2/silver__asset_metadata_coin_gecko2.sql
@@ -0,0 +1,35 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "CONCAT_WS('-', id, coalesce(token_address,''))",
+    incremental_strategy = 'delete+insert',
+    cluster_by = ['_inserted_timestamp::DATE']
+) }}
+
+SELECT
+    id,
+    i.value :: STRING AS token_address,
+    NAME,
+    symbol,
+    i.key AS platform,
+    _inserted_timestamp,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['id','token_address']) }} AS asset_metadata_coin_gecko_id,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    {{ ref('bronze__asset_metadata_coin_gecko') }} A,
+    TABLE(FLATTEN(A.value :platforms)) i
+
+{% if is_incremental() %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp) - INTERVAL '24 hours'
+        FROM
+            {{ this }}
+    )
+{% endif %}
+
+qualify(ROW_NUMBER() over (PARTITION BY id, token_address
+ORDER BY
+    _inserted_timestamp DESC)) = 1
---
+++ models/silver/hourly_prices/coin_gecko2/silver__token_prices_coin_gecko_hourly2.sql
@@ -0,0 +1,168 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "token_prices_coin_gecko_hourly_id",
+    incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
+    cluster_by = ['recorded_hour::DATE']
+) }}
+
+WITH date_hours AS (
+
+    SELECT
+        date_hour
+    FROM
+        {{ ref(
+            'core__dim_date_hours'
+        ) }}
+    WHERE
+        date_hour >= '2018-01-01'
+        AND date_hour <= (
+            SELECT
+                MAX(recorded_hour)
+            FROM
+                {{ ref(
+                    'silver__hourly_prices_coin_gecko2'
+                ) }}
+        )
+
+{% if is_incremental() %}
+AND date_hour >= (
+    SELECT
+        MAX(recorded_hour) - INTERVAL '72 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+asset_metadata AS (
+    SELECT
+        DISTINCT CASE
+            WHEN LOWER(platform) = 'aptos' THEN token_address
+            WHEN TRIM(token_address) ILIKE '^x%'
+            OR TRIM(token_address) ILIKE '0x%' THEN REGEXP_SUBSTR(REGEXP_REPLACE(token_address, '^x', '0x'), '0x[a-zA-Z0-9]*')
+            WHEN id = 'osmosis' THEN 'uosmo'
+            WHEN id = 'algorand' THEN '0'
+            ELSE token_address
+        END AS token_address,
+        id,
+        LOWER(
+            CASE
+                WHEN id = 'osmosis' THEN 'osmosis'
+                WHEN id = 'algorand' THEN 'algorand'
+                WHEN id = 'solana' THEN 'solana'
+                ELSE platform :: STRING
+            END
+        ) AS platform,
+        _inserted_timestamp
+    FROM
+        {{ ref(
+            'silver__asset_metadata_coin_gecko2'
+        ) }}
+),
+base_date_hours_address AS (
+    SELECT
+        date_hour,
+        token_address,
+        id,
+        platform
+    FROM
+        date_hours
+        CROSS JOIN asset_metadata
+),
+base_prices AS (
+    SELECT
+        p.recorded_hour,
+        p._inserted_timestamp,
+        m.token_address,
+        p.id,
+        m.platform,
+        p.close
+    FROM
+        {{ ref(
+            'silver__hourly_prices_coin_gecko2'
+        ) }}
+        p
+        LEFT JOIN asset_metadata m
+        ON m.id = p.id
+
+{% if is_incremental() %}
+AND p._inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '72 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+imputed_prices AS (
+    SELECT
+        --dateadd(hour,1,date_hour) AS date_hour, --if we want to roll the close price forward 1 hour
+        date_hour AS recorded_hour,
+        d.token_address,
+        d.id,
+        d.platform,
+        p.close AS hourly_close,
+        LAST_VALUE(
+            p.close ignore nulls
+        ) over (
+            PARTITION BY d.token_address,
+            d.id,
+            d.platform
+            ORDER BY
+                d.date_hour rows unbounded preceding
+        ) AS imputed_close,
+        COALESCE(
+            hourly_close,
+            imputed_close
+        ) AS final_close,
+        CASE
+            WHEN hourly_close IS NULL THEN TRUE
+            ELSE FALSE
+        END AS imputed,
+        _inserted_timestamp
+    FROM
+        base_date_hours_address d
+        LEFT JOIN base_prices p
+        ON p.recorded_hour = d.date_hour
+        AND p.token_address = d.token_address
+        AND LOWER(
+            p.id
+        ) = LOWER(
+            d.id
+        )
+),
+final_prices AS (
+    SELECT
+        recorded_hour,
+        token_address,
+        id,
+        platform,
+        final_close AS CLOSE,
+        imputed,
+        _inserted_timestamp AS _inserted_timestamp_raw,
+        CASE
+            WHEN imputed THEN SYSDATE()
+            ELSE NULL
+        END AS _imputed_timestamp
+    FROM
+        imputed_prices
+    WHERE CLOSE IS NOT NULL
+)
+SELECT
+    recorded_hour,
+    token_address,
+    platform,
+    id,
+    CLOSE,
+    imputed,
+    COALESCE(
+        _inserted_timestamp_raw,
+        _imputed_timestamp
+    ) AS _inserted_timestamp,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['recorded_hour','token_address','id','platform']) }} AS token_prices_coin_gecko_hourly_id,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    final_prices
+--original model is unique on recorded_hour, token_address, platform NOT INCLUDING id
