dad2f859902b7f086fdc583355bae7d8c36514f0
shah <info@shahnewazkhan.ca>
2024-02-07 14:32:05 -0800

    fixed url param delimter

--- models/streamline/streamline__coingecko_realtime_ohlc.sql
+++ models/streamline/streamline__coingecko_realtime_ohlc.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', '', 'sql_limit', {{var('sql_limit','10')}}, 'producer_batch_size', {{var('producer_batch_size','10')}}, 'worker_batch_size', {{var('worker_batch_size','10')}}, 'sm_secret_name',''))",
+        func = "{{this.schema}}.udf_bulk_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', '', 'sql_limit', {{var('sql_limit','10')}}, 'producer_batch_size', {{var('producer_batch_size','10')}}, 'worker_batch_size', {{var('worker_batch_size','10')}}, 'sm_secret_name','prod/coingecko/rest'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['ohlc_realtime_v2']
@@ -10,7 +10,7 @@
 WITH calls AS (
 
     SELECT
-        '{service}/api/v3/coins/' || asset_id || '/ohlc?vs_currency=usd&days=1,x_cg_pro_api_key={Authentication}' calls,
+        '{service}/api/v3/coins/' || asset_id || '/ohlc?vs_currency=usd&days=1&x_cg_pro_api_key={Authentication}' calls,
         asset_id
     FROM
         (
