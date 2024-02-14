0bb5efe7467162613d3b51103e300a9ffc8c8768
shah <info@shahnewazkhan.ca>
2024-02-13 19:36:26 -0800

    add prod api_integration

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -1,13 +1,20 @@
 -- macro used to create flow api integrations
 {% macro create_aws_crosschain_api() %}
     {% if target.name == "prod" %}
-        {# pass #}
+        {{ log("Generating api integration for target:" ~ target.name, info=True) }}
+        {% set sql %}
+        
+        CREATE api integration IF NOT EXISTS aws_crosschain_api_prod api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::924682671219:role/crosschain-api-prod-rolesnowflakeudfsAF733095-hN5B5eoGh80E' api_allowed_prefixes = (
+            'https://y4vgsb5jk5.execute-api.us-east-1.amazonaws.com/prod/'
+        ) enabled = TRUE;    
+        {% endset %}
+        {% do adapter.execute(sql) %}
     {% elif target.name == "dev" %}
         {{ log("Generating api integration for target:" ~ target.name, info=True) }}
         {% set sql %}
         
-        CREATE api integration IF NOT EXISTS aws_crosschain_api_dev_stg api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::704693948482:role/crosschain-api-dev-rolesnowflakeudfsAF733095-JKJPFBDhuynL' api_allowed_prefixes = (
-            'https://skzzs32zdb.execute-api.us-east-1.amazonaws.com/dev/'
+        CREATE api integration IF NOT EXISTS aws_crosschain_api_stg api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::704693948482:role/crosschain-api-stg-rolesnowflakeudfsAF733095-5u61kmZdVfGr' api_allowed_prefixes = (
+            'https://q0bnjqvs9a.execute-api.us-east-1.amazonaws.com/dev/'
         ) enabled = TRUE;    
         {% endset %}
         {% do adapter.execute(sql) %}
@@ -22,3 +29,7 @@
         {% do run_query(sql) %}
     {% endif %}
 {% endmacro %}
+
+
+
+
