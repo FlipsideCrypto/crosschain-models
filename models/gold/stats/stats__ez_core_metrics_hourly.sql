e75837e737d467efc8f88c726bf0d809b451b67e
drethereum <71602799+drethereum@users.noreply.github.com>
2024-02-27 13:55:46 -0700

    Add/metrics-core-hrly (#251)
    * naming
    
    * union and docs
    
    * stats sources
    
    * alias
    
    * table
    
    * comment
    
    * update source
    
    * role
    
    * complete stats model
    
    * all chains
    
    * columns
    
    * null

---
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true },
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'STATS, METRICS, CORE, HOURLY',
+    } } }
+) }}
+
+SELECT
+    blockchain,
+    block_timestamp_hour,
+    block_number_min,
+    block_number_max,
+    block_count,
+    transaction_count,
+    transaction_count_success,
+    transaction_count_failed,
+    unique_initiator_count,
+    total_fees_usd,
+    core_metrics_hourly_id AS ez_core_metrics_hourly_id,
+    inserted_timestamp,
+    modified_timestamp
+FROM
+    {{ ref('silver__complete_core_metrics_hourly') }}
