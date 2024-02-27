bb4760540b5c54a37838ad995f9a7cfb25a17b56
Graham Onders <grahamonders@gmail.com>
2024-02-27 14:12:01 -0700

    solana inscription tags (#270)
    * solana inscription tags
    
    * fix tag id name

---
+++ models/silver/tags/Solana/silver__libreplex_inscription.sql
@@ -0,0 +1,68 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "CONCAT_WS('-', address, start_date, tag_name)",
+    incremental_strategy = 'delete+insert',
+) }}
+
+WITH libreplex_txs AS (
+
+    SELECT
+        block_timestamp,
+        f_inner.value :accounts [2] :: STRING AS non_fungible_mint
+    FROM
+        {{ source(
+            'solana_silver',
+            'events'
+        ) }}
+        fe
+        INNER JOIN LATERAL FLATTEN (
+            input => inner_instruction :instructions
+        ) f_inner
+    WHERE
+        succeeded
+        AND ARRAY_CONTAINS(
+            'inscokhJarcjaEs59QbQ7hYjrKz25LEPRfCbP8EmdUp' :: variant,
+            instruction :accounts
+        )
+        AND f_inner.value :programId = 'inscokhJarcjaEs59QbQ7hYjrKz25LEPRfCbP8EmdUp'
+        AND SUBSTR(
+            livequery.utils.udf_base58_to_hex(
+                f_inner.value :data
+            ),
+            1,
+            18
+        ) IN (
+            '0x893cf85eabccc360', -- CreateInscription,
+            '0x148e6512785a0975', -- CreateInscriptionV2,
+            '0xa82b4de6f0f7a1af' -- CreateInscriptionV3
+        )
+
+{% if is_incremental() %}
+AND _inserted_timestamp :: DATE >= (
+    SELECT
+        MAX(
+            inserted_timestamp
+        )
+    FROM
+        {{ this }}
+)
+{% else %}
+    AND _inserted_timestamp :: DATE > '2023-11-13'
+{% endif %}
+)
+SELECT
+    'solana' AS blockchain,
+    'marqu' AS creator,
+    non_fungible_mint AS address,
+    'libreplex' AS tag_name,
+    'inscription' AS tag_type,
+    block_timestamp AS start_date,
+    NULL AS end_date,
+    SYSDATE() AS tag_created_at,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    {{ dbt_utils.generate_surrogate_key(['address','tag_name','start_date']) }} AS tags_libreplex_id,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    libreplex_txs
+
