b8e41e032731ce12957c2bef300892dd3e75212d
drethereum <trevor.wenokur@gmail.com>
2024-03-13 12:29:16 -0600

    gap test

--- macros/tests/sequence_gaps.sql
+++ macros/tests/sequence_gaps.sql
@@ -32,3 +32,42 @@ WHERE
     {{ column_name }} - {{ previous_column }} <> 1
 ORDER BY
     gap DESC {% endtest %}
+
+{% test hour_sequence_gaps(
+            model,
+            partition_by,
+            column_name
+        ) %}
+        {%- set partition_sql = partition_by | join(", ") -%}
+        {%- set previous_column = "prev_" ~ column_name -%}
+        WITH source AS (
+            SELECT
+                {{ partition_sql }},
+                {{ column_name }},
+                LAG(
+                    {{ column_name }},
+                    1
+                ) over (
+                    {{ "PARTITION BY " ~ partition_sql if partition_sql }}
+                    ORDER BY
+                        {{ column_name }} ASC
+                ) AS {{ previous_column }}
+            FROM
+                {{ model }}
+        )
+    SELECT
+        {{ partition_sql }},
+        {{ previous_column }},
+        {{ column_name }},
+        DATEDIFF(
+            HOUR,
+            {{ previous_column }},
+            {{ column_name }}
+        ) - 1 AS gap
+    FROM
+        source
+    WHERE
+        gap <> 0
+    ORDER BY
+        gap DESC
+{% endtest %}
