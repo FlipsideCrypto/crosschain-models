48c7c426a57f9360e23eb2b05d39b85fa6e5bb0d
drethereum <71602799+drethereum@users.noreply.github.com>
2024-03-13 17:36:58 -0600

    fix/cg-complete-rt (#293)
    * tweaks
    
    * workers

--- models/streamline/silver/coingecko/prices/streamline__hourly_prices_coingecko_history.sql
+++ models/streamline/silver/coingecko/prices/streamline__hourly_prices_coingecko_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_MARKET_CHART_API/COINGECKO', 'sql_limit', {{var('sql_limit','50000')}}, 'producer_batch_size', {{var('producer_batch_size','50000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}, 'sm_secret_name','prod/coingecko/rest'))",
+        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_MARKET_CHART_API/COINGECKO', 'sql_limit', {{var('sql_limit','50000')}}, 'producer_batch_size', {{var('producer_batch_size','50000')}}, 'worker_batch_size', {{var('worker_batch_size','12500')}}, 'sm_secret_name','prod/coingecko/rest'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_cg_prices_history']
@@ -24,7 +24,7 @@ WITH assets AS (
 run_times AS (
     SELECT
         asset_id,
-        run_time :: DATE AS run_time
+        run_time :: DATE AS runtime
     FROM
         assets A
         CROSS JOIN {{ ref('streamline__runtimes_daily') }}
@@ -36,22 +36,22 @@ run_times AS (
     EXCEPT
     SELECT
         id AS asset_id,
-        run_time :: DATE AS run_time
+        recorded_date :: DATE AS runtime
     FROM
         {{ ref('streamline__hourly_prices_coingecko_complete') }}
     WHERE
-        run_time :: DATE < DATEADD('day', -1, SYSDATE())
+        recorded_date :: DATE < DATEADD('day', -1, SYSDATE())
         ),
         calls AS (
             SELECT
                 asset_id,
                 DATE_PART(
                     'EPOCH',
-                    run_time :: DATE
+                    runtime :: DATE
                 ) :: INTEGER AS start_time_epoch,
                 DATE_PART(
                     'EPOCH',
-                    DATEADD('day',1,run_time :: DATE)
+                    DATEADD('day',1,runtime :: DATE)
                 ) :: INTEGER AS end_time_epoch,
                 '{service}/api/v3/coins/' || asset_id || '/market_chart/range?vs_currency=usd&from=' || start_time_epoch || '&to=' || end_time_epoch || '&x_cg_pro_api_key={Authentication}' AS api_url
             FROM
