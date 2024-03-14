0f30a4b0a265ea3b0ebdc0d49bab6a1118927e8b
drethereum <71602799+drethereum@users.noreply.github.com>
2024-03-14 16:20:31 -0600

    add tasks (#296)

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,11 +2,6 @@
     {% if var("UPDATE_UDFS_AND_SPS", False) %}
         {% set sql %}
             CREATE schema if NOT EXISTS streamline;
-            {{ udf_bulk_get_coin_gecko_asset_metadata() }};
-            {{ () }};
-            {{ udf_bulk_get_coin_market_cap_hourly_ohlc() }};
-            {{ udf_bulk_get_coin_gecko_ohlc() }};
-            {{ udf_bulk_get_coin_gecko_asset_market_data_historical() }};
             {{ create_udf_hex_to_int(
             schema = "public"
         ) }}
