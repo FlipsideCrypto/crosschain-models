6804fc5ff1049e3535790265c2fbbfbae8e55930
drethereum <trevor.wenokur@gmail.com>
2024-03-13 21:43:26 -0600

    new history model and tasks

--- models/streamline/silver/coingecko/prices/streamline__hourly_prices_coingecko_history.sql
+++ models/streamline/silver/coingecko/prices/streamline__hourly_prices_coingecko_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_MARKET_CHART_API/COINGECKO', 'sql_limit', {{var('sql_limit','50000')}}, 'producer_batch_size', {{var('producer_batch_size','50000')}}, 'worker_batch_size', {{var('worker_batch_size','12500')}}, 'sm_secret_name','prod/coingecko/rest'))",
+        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_MARKET_CHART_API/COINGECKO', 'sql_limit', {{var('sql_limit','2000')}}, 'producer_batch_size', {{var('producer_batch_size','2000')}}, 'worker_batch_size', {{var('worker_batch_size','2000')}}, 'sm_secret_name','prod/coingecko/rest'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_cg_prices_history']
@@ -24,41 +24,34 @@ WITH assets AS (
 run_times AS (
     SELECT
         asset_id,
-        run_time :: DATE AS runtime
+        run_time
     FROM
-        assets A
+        assets
         CROSS JOIN {{ ref('streamline__runtimes_daily') }}
     WHERE
-        run_time :: DATE BETWEEN '2024-01-15' :: DATE
-        AND DATEADD('day', -1, SYSDATE()) -- temp logic for backfill
-        -- run_time :: DATE BETWEEN DATEADD('day',-{{ var("LOOKBACK", 45) }}, SYSDATE())
-        -- AND DATEADD('day', -1, SYSDATE()) -- long term logic
+        run_time >= DATEADD('day', -91, SYSDATE())
+        AND run_time <= DATEADD('day', -1, SYSDATE())
     EXCEPT
     SELECT
         id AS asset_id,
-        recorded_date :: DATE AS runtime
+        recorded_date AS run_time
     FROM
         {{ ref('streamline__hourly_prices_coingecko_complete') }}
     WHERE
-        recorded_date :: DATE < DATEADD('day', -1, SYSDATE())
-        ),
-        calls AS (
-            SELECT
-                asset_id,
-                DATE_PART(
-                    'EPOCH',
-                    runtime :: DATE
-                ) :: INTEGER AS start_time_epoch,
-                DATE_PART(
-                    'EPOCH',
-                    DATEADD('day',1,runtime :: DATE)
-                ) :: INTEGER AS end_time_epoch,
-                '{service}/api/v3/coins/' || asset_id || '/market_chart/range?vs_currency=usd&from=' || start_time_epoch || '&to=' || end_time_epoch || '&x_cg_pro_api_key={Authentication}' AS api_url
-            FROM
-                run_times
-        )
+        run_time >= DATEADD('day', -91, SYSDATE())
+        AND run_time <= DATEADD('day', -1, SYSDATE())
+),
+calls AS (
     SELECT
-        start_time_epoch AS partition_key,
+        DISTINCT asset_id,
+        '{service}/api/v3/coins/' || asset_id || '/market_chart?vs_currency=usd&days=90&interval=hourly&precision=full&x_cg_pro_api_key={Authentication}' AS api_url
+    FROM
+        run_times
+)
+SELECT
+    DATE_PART(
+        'EPOCH',
+        DATEADD('day', -91, SYSDATE()) :: DATE) AS partition_key,
         ARRAY_CONSTRUCT(
             partition_key,
             ARRAY_CONSTRUCT(
@@ -69,7 +62,7 @@ run_times AS (
                 ''
             )
         ) AS request
-    FROM
-        calls
-    ORDER BY
-        partition_key ASC
+FROM
+    calls
+ORDER BY
+    partition_key ASC
