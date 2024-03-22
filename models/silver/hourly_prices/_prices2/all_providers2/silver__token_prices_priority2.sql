b9da40e1c966d0031514acd55e9f96a40bc697b0
drethereum <trevor.wenokur@gmail.com>
2024-03-22 17:30:03 -0600

    qualify

--- models/silver/hourly_prices/_prices2/all_providers2/silver__token_prices_priority2.sql
+++ models/silver/hourly_prices/_prices2/all_providers2/silver__token_prices_priority2.sql
@@ -1,6 +1,6 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['hour','token_address','blockchain_id'],
+    unique_key = ['hour','token_address','blockchain'],
     incremental_strategy = 'delete+insert',
     cluster_by = ['hour::DATE'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION on equality(token_address, hour, blockchain)",
@@ -59,7 +59,7 @@ SELECT
     _inserted_timestamp,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['hour','token_address','blockchain_id']) }} AS token_prices_priority_hourly_id,
+    {{ dbt_utils.generate_surrogate_key(['hour','token_address','blockchain']) }} AS token_prices_priority_hourly_id,
     '{{ invocation_id }}' AS _invocation_id
 FROM
     all_providers qualify(ROW_NUMBER() over (PARTITION BY HOUR, token_address, blockchain
