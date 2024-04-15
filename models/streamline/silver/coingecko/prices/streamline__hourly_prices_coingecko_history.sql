2abf63e2978eae8a37f8fc5d515e355605e7d521
drethereum <trevor.wenokur@gmail.com>
2024-04-15 16:27:11 -0600

    revert

--- models/streamline/silver/coingecko/prices/streamline__hourly_prices_coingecko_history.sql
+++ models/streamline/silver/coingecko/prices/streamline__hourly_prices_coingecko_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_MARKET_CHART_API/COINGECKO', 'sql_limit', {{var('sql_limit','1000')}}, 'producer_batch_size', {{var('producer_batch_size','1000')}}, 'worker_batch_size', {{var('worker_batch_size','1000')}}))",
+        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_MARKET_CHART_API/COINGECKO', 'sql_limit', {{var('sql_limit','1000')}}, 'producer_batch_size', {{var('producer_batch_size','1000')}}, 'worker_batch_size', {{var('worker_batch_size','1000')}}, 'sm_secret_name','prod/coingecko/rest'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_cg_prices_history']
@@ -56,12 +56,15 @@ SELECT
     DATE_PART(
         'EPOCH',
         DATEADD('day', -91, SYSDATE()) :: DATE) AS partition_key,
-        {{ target.database }}.live.udf_api(
-            'GET',
-            api_url,
-            NULL,
-            NULL,
-            'vault/prod/coingecko/rest'
+        ARRAY_CONSTRUCT(
+            partition_key,
+            ARRAY_CONSTRUCT(
+                'GET',
+                api_url,
+                PARSE_JSON('{}'),
+                PARSE_JSON('{}'),
+                ''
+            )
         ) AS request
 FROM
     calls
--- models/streamline/silver/coinmarketcap/asset_metadata/streamline__asset_metadata_coinmarketcap.sql
+++ models/streamline/silver/coinmarketcap/asset_metadata/streamline__asset_metadata_coinmarketcap.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_METADATA_API/COINMARKETCAP', 'sql_limit', {{var('sql_limit','10')}}, 'producer_batch_size', {{var('producer_batch_size','10')}}, 'worker_batch_size', {{var('worker_batch_size','10')}}))",
+        func = "{{this.schema}}.udf_bulk_rest_api_v2(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'ASSET_METADATA_API/COINMARKETCAP', 'sql_limit', {{var('sql_limit','10')}}, 'producer_batch_size', {{var('producer_batch_size','10')}}, 'worker_batch_size', {{var('worker_batch_size','10')}}, 'sm_secret_name','prod/prices/coinmarketcap'))",
         target = "{{this.schema}}.{{this.identifier}}"
     ),
     tags = ['streamline_cmc_asset_metadata']
@@ -14,12 +14,15 @@ WITH calls AS (
 )
 SELECT
     DATE_PART('EPOCH', SYSDATE()) :: INTEGER AS partition_key,
-    {{ target.database }}.live.udf_api(
-        'GET',
-        api_url,
-        PARSE_JSON('{"Accept": "application/json", "Accept-Encoding": "deflate, gzip", "X-CMC_PRO_API_KEY": "{Authentication}"}'),
-        NULL,
-        'vault/prod/prices/coinmarketcap'
+    ARRAY_CONSTRUCT(
+        partition_key,
+        ARRAY_CONSTRUCT(
+            'GET',
+            api_url,
+            PARSE_JSON('{"Accept": "application/json", "Accept-Encoding": "deflate, gzip", "X-CMC_PRO_API_KEY": "{Authentication}"}'),
+            PARSE_JSON('{}'),
+            ''
+        ) 
     ) AS request
 FROM
     calls 
