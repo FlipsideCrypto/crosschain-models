9dde8957de2b4da5a856dd93bc288127d5147333
drethereum <71602799+drethereum@users.noreply.github.com>
2024-04-15 15:54:22 -0600

    Fix/bronze-cg-value-structure (#322)
    * URI and bronze structure
    
    * URI
    
    * Update streamline__asset_metadata_coingecko.sql to include exploded_key in udf_bulk_rest_api_v2 call
    
    * spacing
    
    * Update streamline__asset_metadata_coingecko.sql to fix udf_bulk_rest_api_v2 call
    
    ---------
    
    Co-authored-by: Julius Remigio <14811322+juls858@users.noreply.github.com>

--- models/streamline/bronze/coingecko/asset_metadata/bronze__streamline_asset_metadata_coingecko.sql
+++ models/streamline/bronze/coingecko/asset_metadata/bronze__streamline_asset_metadata_coingecko.sql
@@ -34,20 +34,50 @@ asset_metadata AS (
         s
         JOIN meta b
         ON s.metadata$filename = b.file_name
+),
+type1 AS (
+    SELECT
+        f.value AS VALUE,
+        'coingecko' AS provider,
+        f.value :id :: STRING AS id,
+        f.value :symbol :: STRING AS symbol,
+        f.value :name :: STRING AS NAME,
+        run_time,
+        _inserted_date,
+        _inserted_timestamp
+    FROM
+        asset_metadata A,
+        LATERAL FLATTEN(
+            input => DATA
+        ) f
+    WHERE
+        _inserted_timestamp < '2024-04-15 20:25:36.000 +0000'
+        -- prior to Quantum upgrade
+),
+type2 AS (
+    SELECT
+        DATA AS VALUE,
+        'coingecko' AS provider,
+        DATA :id :: STRING AS id,
+        DATA :symbol :: STRING AS symbol,
+        DATA :name :: STRING AS NAME,
+        run_time,
+        _inserted_date,
+        _inserted_timestamp
+    FROM
+        asset_metadata
+    WHERE
+        _inserted_timestamp >= '2024-04-15 20:25:36.000 +0000'
+        -- post Quantum upgrade
 )
 SELECT
-    f.value AS VALUE,
-    'coingecko' AS provider,
-    f.value :id :: STRING AS id,
-    f.value :symbol :: STRING AS symbol,
-    f.value :name :: STRING AS NAME,
-    run_time,
-    _inserted_date,
-    _inserted_timestamp
+    *
 FROM
-    asset_metadata A,
-    LATERAL FLATTEN(
-        input => DATA
-    ) f 
+    type1
+UNION ALL
+SELECT
+    *
+FROM
+    type2 
     -- streamline 2.0 external table
     -- columns parsed out to match legacy bronze model `bronze__asset_metadata_coin_gecko`
