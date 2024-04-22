3368c0bb11ace4462b88451a972cac1b5a4f25e6
drethereum <71602799+drethereum@users.noreply.github.com>
2024-04-22 13:57:47 -0600

    add/new-native-tokens1 (#334)
    * new native assets
    
    * updated name and id

--- models/silver/prices/complete/silver__complete_provider_prices.sql
+++ models/silver/prices/complete/silver__complete_provider_prices.sql
@@ -76,7 +76,7 @@ SELECT
     _inserted_timestamp,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
-    {{ dbt_utils.generate_surrogate_key(['asset_id','recorded_hour']) }} AS complete_provider_prices_id,
+    {{ dbt_utils.generate_surrogate_key(['asset_id','recorded_hour','provider']) }} AS complete_provider_prices_id,
     '{{ invocation_id }}' AS _invocation_id
 FROM
     all_providers qualify(ROW_NUMBER() over(PARTITION BY asset_id, recorded_hour, provider
