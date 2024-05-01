2ca5a2bb2bfad8e94e193c9f18a4bfa6866e6630
drethereum <trevor.wenokur@gmail.com>
2024-05-01 12:15:00 -0600

    fix for compile bug

--- models/silver/defi/silver__complete_dex_swaps.sql
+++ models/silver/defi/silver__complete_dex_swaps.sql
@@ -369,11 +369,11 @@ solana AS (
         tx_id AS tx_hash,
         program_id AS contract_address,
         swapper AS trader,
-        LOWER(swap_from_mint) AS token_in,
+        swap_from_mint AS token_in,
         NULL AS symbol_in,
         swap_from_amount AS amount_in_raw,
         amount_in_raw AS amount_in,
-        LOWER(swap_to_mint) AS token_out,
+        swap_to_mint AS token_out,
         NULL AS symbol_out,
         swap_to_amount AS amount_out_raw,
         amount_out_raw AS amount_out,
@@ -490,6 +490,18 @@ all_chains_dex AS (
         *
     FROM
         near
+),
+prices AS (
+    SELECT
+        HOUR,
+        token_address,
+        symbol,
+        decimals,
+        price,
+        blockchain
+    FROM
+        {{ ref('price__ez_prices_hourly') }}
+    WHERE NOT IS_NATIVE
 )
 SELECT
     d.blockchain,
@@ -529,7 +541,7 @@ SELECT
     d._unique_key
 FROM
     all_chains_dex d
-    LEFT JOIN {{ ref('price__ez_hourly_token_prices') }}
+    LEFT JOIN prices
     p_in
     ON REPLACE(
         d.blockchain,
@@ -541,7 +553,7 @@ FROM
         'hour',
         d.block_timestamp
     ) = p_in.hour
-    LEFT JOIN {{ ref('price__ez_hourly_token_prices') }}
+    LEFT JOIN prices
     p_out
     ON REPLACE(
         d.blockchain,
