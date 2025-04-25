44ef5132ec80d96ece263200bbc243d1e42a3826
drethereum <trevor.wenokur@gmail.com>
2025-04-25 13:18:13 -0600

    comment

--- macros/prices/update_blockchain.sql
+++ macros/prices/update_blockchain.sql
@@ -1,5 +1,11 @@
 {% macro update_blockchain(new_blockchain_name, current_blockchain_names) %}
-
+{#
+    This macro updates the blockchain column in the prices tables. 
+    It requires a corresponding update in the all_providers tables to normalize the blockchain names.
+    It takes two arguments:
+    - new_blockchain_name: The new blockchain name to set in the blockchain column.
+    - current_blockchain_names: A list of current blockchain names to update.
+#}
 {% for table in [
     'crosschain.silver.token_prices_all_providers',
     'crosschain.silver.token_asset_metadata_all_providers',
