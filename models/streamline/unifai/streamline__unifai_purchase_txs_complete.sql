a4e15961140748d71cf9b2e96aa9b31bb99d8a83
tarikceric <46071768+tarikceric@users.noreply.github.com>
2025-05-06 15:23:11 -0700

    unifai purchase txs (#442)

---
+++ models/streamline/unifai/streamline__unifai_purchase_txs_complete.sql
@@ -0,0 +1,24 @@
+{{ config (
+    materialized = "incremental",
+    unique_key = ['partition_key'],
+    cluster_by = "partition_key"
+) }}
+
+SELECT
+    partition_key,
+    TO_TIMESTAMP_NTZ(partition_key, 'YYYY_MM_DD') as run_date,
+    max(_inserted_timestamp) as _inserted_timestamp
+FROM
+    {{ ref('bronze__streamline_unifai_purchase_txs') }}
+
+{% if is_incremental() %}
+where _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp)
+    FROM
+        {{ this }}
+)
+{% endif %}
+GROUP BY 1,2
+
+
