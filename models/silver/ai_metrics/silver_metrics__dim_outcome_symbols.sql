974ade1de89a3fd74d81aabda8fd98520e2c01a2
mattromano <mattromano88@gmail.com>
2025-05-29 14:44:43 -0700

    fix refresh to only when there's new platforms

--- models/silver/ai_metrics/silver_metrics__dim_outcome_symbols.sql
+++ models/silver/ai_metrics/silver_metrics__dim_outcome_symbols.sql
@@ -1,193 +1,239 @@
-  -- depends_on: {{ ref('defi__dim_dex_liquidity_pools') }}
-  -- depends_on: {{ ref('defi__ez_dex_swaps') }}
-  -- depends_on: {{ ref('defi__fact_bridge_activity') }}
+-- depends_on: {{ ref('defi__dim_dex_liquidity_pools') }}
+-- depends_on: {{ ref('silver__tokens') }}
+-- depends_on: {{ ref('defi__ez_dex_swaps') }}
+-- depends_on: {{ ref('defi__fact_bridge_activity') }}
 
 {{ config(
-    materialized = 'incremental',
-    unique_key = ['blockchain', 'platform', 'action'],
-    merge_exclude_columns = ['inserted_timestamp'],
+    materialized = 'table',
     tags = ['daily']
 ) }}
 
-{% if is_incremental() %}
-{% set max_mod_query %}
-SELECT MAX(modified_timestamp) :: DATE AS modified_timestamp
-FROM {{ this }}
-{% endset %}
-{% set max_mod = run_query(max_mod_query)[0][0] %}
+{%- set should_run = true %}
 
-{% set new_check_query %}
-WITH check_new_combinations AS (
-    SELECT DISTINCT blockchain, platform, action 
-    FROM (
-        SELECT blockchain, platform, 'lp' as action 
-        FROM {{ ref('defi__dim_dex_liquidity_pools') }}
-        WHERE modified_timestamp :: DATE >= '{{ max_mod }}'
-        UNION
-        SELECT blockchain, platform, 'swap' as action 
-        FROM {{ ref('defi__ez_dex_swaps') }}
-        WHERE modified_timestamp :: DATE >= '{{ max_mod }}'
-        UNION
-        SELECT blockchain, platform, 'bridge' as action 
-        FROM {{ ref('defi__fact_bridge_activity') }}
-        WHERE modified_timestamp :: DATE >= '{{ max_mod }}'
-    ) a
-    EXCEPT
-    SELECT DISTINCT blockchain, platform, action
-    FROM {{ this }}
-)
+{% if execute and not flags.FULL_REFRESH %}
+    {%- set check_query %}
+        WITH last_modified AS (
+            SELECT MAX(modified_timestamp)::DATE AS last_mod 
+            FROM {{ this }}
+        ),
+        check_new_combinations AS (
+            SELECT DISTINCT 
+                blockchain,
+                platform,
+                action
+            FROM (
+                SELECT 
+                    blockchain,
+                    platform,
+                    'lp' AS action
+                FROM {{ ref('defi__dim_dex_liquidity_pools') }}
+                WHERE modified_timestamp::DATE >= (SELECT last_mod FROM last_modified)
+                
+                UNION
+                
+                SELECT 
+                    blockchain,
+                    platform,
+                    'swap' AS action
+                FROM {{ ref('defi__ez_dex_swaps') }}
+                WHERE modified_timestamp::DATE >= (SELECT last_mod FROM last_modified)
+                
+                UNION
+                
+                SELECT 
+                    blockchain,
+                    platform,
+                    'bridge' AS action
+                FROM {{ ref('defi__fact_bridge_activity') }}
+                WHERE modified_timestamp::DATE >= (SELECT last_mod FROM last_modified)
+            ) A
+            EXCEPT
+            SELECT DISTINCT
+                blockchain,
+                platform,
+                action
+            FROM {{ this }}
+        )
+        SELECT COUNT(*) AS new_count
+        FROM check_new_combinations
+    {% endset %}
 
-SELECT COUNT(*) FROM check_new_combinations
-{% endset %}
-{% set new_count = run_query(new_check_query)[0][0] %}
+    {%- set results = run_query(check_query) %}
+    {%- if results[0][0] == 0 %}
+        {%- set should_run = false %}
+        {{ log("No new combinations found - using existing table (use --full-refresh to override)", info=true) }}
+        SELECT * FROM {{ this }}
+    {%- endif %}
+{% endif %}
 
-{% if new_count == 0 %}
-    -- Return empty result to skip processing
-    SELECT
-        blockchain,
-        platform,
-        action,
-        top_symbols,
-        inserted_timestamp,
-        modified_timestamp,
-        outcome_symbols_id,
-        _invocation_id
-    FROM {{ this }}
-    WHERE 1=0
-
-{% else %}
+{% if should_run %}
 WITH symbols_by_lp AS (
-    SELECT 
+    SELECT
         l.blockchain,
         l.platform,
-        'lp' as action,
+        'lp' AS action,
         l.pool_name,
         l.pool_address,
-        SUM(COALESCE(s.amount_in_usd, 0)) as volume_usd,
-        ROW_NUMBER() OVER (PARTITION BY l.blockchain, l.platform ORDER BY SUM(COALESCE(s.amount_in_usd, 0)) DESC) as rn
+        SUM(COALESCE(s.amount_in_usd, 0)) AS volume_usd,
+        ROW_NUMBER() OVER (
+            PARTITION BY l.blockchain, l.platform
+            ORDER BY SUM(COALESCE(s.amount_in_usd, 0)) DESC
+        ) AS rn
     FROM {{ ref('defi__dim_dex_liquidity_pools') }} l
     LEFT JOIN {{ ref('defi__ez_dex_swaps') }} s
-        ON l.blockchain = s.blockchain 
+        ON l.blockchain = s.blockchain
         AND l.pool_address = s.contract_address
-    GROUP BY l.blockchain, l.platform, l.pool_name, l.pool_address
+    GROUP BY
+        l.blockchain,
+        l.platform,
+        l.pool_name,
+        l.pool_address
     QUALIFY rn <= 5
 ),
 
 lp_symbols_agg AS (
-    SELECT 
+    SELECT
         blockchain,
         platform,
         action,
         OBJECT_AGG(
-            'rank_' || rn::string, 
+            'rank_' || rn::STRING,
             OBJECT_CONSTRUCT(
                 'pool_name', pool_name,
                 'pool_address', pool_address,
                 'volume_usd', volume_usd
             )
-        ) as top_symbols
+        ) AS top_symbols
     FROM symbols_by_lp
-    GROUP BY blockchain, platform, action
+    GROUP BY
+        blockchain,
+        platform,
+        action
 ),
 
 symbols_by_swap AS (
-    SELECT 
+    SELECT
         blockchain,
         platform,
-        'swap' as action,
+        'swap' AS action,
         symbol_in,
         symbol_out,
-        SUM(COALESCE(amount_in_usd, 0)) as volume_usd,
-        ROW_NUMBER() OVER (PARTITION BY blockchain, platform ORDER BY SUM(COALESCE(amount_in_usd, 0)) DESC) as rn
+        SUM(COALESCE(amount_in_usd, 0)) AS volume_usd,
+        ROW_NUMBER() OVER (
+            PARTITION BY blockchain, platform
+            ORDER BY SUM(COALESCE(amount_in_usd, 0)) DESC
+        ) AS rn
     FROM {{ ref('defi__ez_dex_swaps') }}
-    GROUP BY blockchain, platform, symbol_in, symbol_out
+    GROUP BY
+        blockchain,
+        platform,
+        symbol_in,
+        symbol_out
     QUALIFY rn <= 5
 ),
 
 swap_symbols_agg AS (
-    SELECT 
+    SELECT
         blockchain,
         platform,
         action,
         OBJECT_AGG(
-            'rank_' || rn::string, 
+            'rank_' || rn::STRING,
             OBJECT_CONSTRUCT(
                 'symbols', ARRAY_CONSTRUCT(symbol_in, symbol_out),
                 'volume_usd', volume_usd
             )
-        ) as top_symbols
+        ) AS top_symbols
     FROM symbols_by_swap
-    GROUP BY blockchain, platform, action
+    GROUP BY
+        blockchain,
+        platform,
+        action
 ),
 
 symbols_by_bridge AS (
-    SELECT 
+    SELECT
         b.blockchain,
         b.platform,
-        'bridge' as action,
+        'bridge' AS action,
         b.direction,
         t.symbol,
-        COUNT(*) as symbol_count,
-        ROW_NUMBER() OVER (PARTITION BY b.blockchain, b.platform, b.direction ORDER BY COUNT(*) DESC) as rn
+        COUNT(*) AS symbol_count,
+        ROW_NUMBER() OVER (
+            PARTITION BY b.blockchain, b.platform, b.direction
+            ORDER BY COUNT(*) DESC
+        ) AS rn
     FROM {{ ref('defi__fact_bridge_activity') }} b
     LEFT JOIN {{ ref('silver__tokens') }} t
         ON b.token_address = t.address
         AND b.blockchain = t.blockchain
-    GROUP BY b.blockchain, b.platform, b.direction, t.symbol
+    GROUP BY
+        b.blockchain,
+        b.platform,
+        b.direction,
+        t.symbol
     QUALIFY rn <= 5
 ),
 
 bridge_symbols_agg AS (
     WITH inbound_agg AS (
-        SELECT 
+        SELECT
             blockchain,
             platform,
             action,
             OBJECT_AGG(
-                'rank_' || rn::string, 
+                'rank_' || rn::STRING,
                 OBJECT_CONSTRUCT(
                     'symbol', symbol,
                     'count', symbol_count
                 )
-            ) as inbound_symbols
+            ) AS inbound_symbols
         FROM symbols_by_bridge
         WHERE direction = 'inbound'
-        GROUP BY blockchain, platform, action
+        GROUP BY
+            blockchain,
+            platform,
+            action
     ),
+
     outbound_agg AS (
-        SELECT 
+        SELECT
             blockchain,
             platform,
             action,
             OBJECT_AGG(
-                'rank_' || rn::string, 
+                'rank_' || rn::STRING,
                 OBJECT_CONSTRUCT(
                     'symbol', symbol,
                     'count', symbol_count
                 )
-            ) as outbound_symbols
+            ) AS outbound_symbols
         FROM symbols_by_bridge
         WHERE direction = 'outbound'
-        GROUP BY blockchain, platform, action
+        GROUP BY
+            blockchain,
+            platform,
+            action
     )
-    SELECT 
-        COALESCE(i.blockchain, o.blockchain) as blockchain,
-        COALESCE(i.platform, o.platform) as platform,
-        COALESCE(i.action, o.action) as action,
+
+    SELECT
+        COALESCE(i.blockchain, o.blockchain) AS blockchain,
+        COALESCE(i.platform, o.platform) AS platform,
+        COALESCE(i.action, o.action) AS action,
         OBJECT_CONSTRUCT(
             'inbound', i.inbound_symbols,
             'outbound', o.outbound_symbols
-        ) as top_symbols
+        ) AS top_symbols
     FROM inbound_agg i
     FULL OUTER JOIN outbound_agg o
-        ON i.blockchain = o.blockchain 
+        ON i.blockchain = o.blockchain
         AND i.platform = o.platform
         AND i.action = o.action
 ),
 
 combined_symbols AS (
     SELECT * FROM lp_symbols_agg
-    UNION ALL 
+    UNION ALL
     SELECT * FROM swap_symbols_agg
     UNION ALL
     SELECT * FROM bridge_symbols_agg
@@ -202,6 +248,5 @@ SELECT
     SYSDATE() AS modified_timestamp,
     {{ dbt_utils.generate_surrogate_key(['blockchain', 'platform', 'action']) }} AS outcome_symbols_id,
     '{{ invocation_id }}' AS _invocation_id
-FROM combined_symbols 
-{% endif %}
+FROM combined_symbols
 {% endif %}
