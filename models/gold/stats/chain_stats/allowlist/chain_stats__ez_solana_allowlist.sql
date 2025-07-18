a57aea364c5ba8058efc3bc863be8542c757ba7a
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-07-18 14:53:10 -0400

    solana views

---
+++ models/gold/stats/chain_stats/allowlist/chain_stats__ez_solana_allowlist.sql
@@ -0,0 +1,36 @@
+{{ config(
+    materialized = 'view',
+    tags = ['metrics_daily']
+) }}
+
+SELECT
+    blockchain,
+    A.address AS token_address,
+    A.symbol,
+    CASE
+        WHEN b.token_address IS NOT NULL THEN 'USD'
+        WHEN C.tracks_asset IS NOT NULL THEN C.tracks_asset
+        ELSE 'alt-token'
+    END AS tracks_asset
+FROM
+    {{ ref('silver__tokens_enhanced') }} A
+    LEFT JOIN (
+        SELECT
+            DISTINCT token_address
+        FROM
+            {{ ref('silver__tokens_stablecoins') }}
+    ) b
+    ON A.address = b.token_address
+    LEFT JOIN (
+        SELECT
+            DISTINCT token_address,
+            tracks_asset
+        FROM
+            {{ ref('silver__tokens_majors') }}
+    ) C
+    ON A.address = C.token_address
+WHERE
+    LOWER(
+        A.blockchain
+    ) = 'solana'
+    AND A.is_verified
