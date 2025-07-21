057927399259d98d110efff20c9031697f023bd0
Austin <93135983+austinFlipside@users.noreply.github.com>
2025-07-21 11:22:53 -0400

    verified contracts (#489)
    * verified contracts
    
    * cluster

---
+++ models/silver/ai_metrics/silver__verified_contracts.sql
@@ -0,0 +1,49 @@
+{{ config (
+    materialized = "table",
+    tags = ['daily'],
+    cluster_by = ["blockchain"],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(contract_address)"
+) }}
+
+with base as (
+    
+select distinct 
+    blockchain, 
+    platform,
+    bridge_address as contract_address,
+    'bridge' as contract_type
+from 
+    {{ ref('defi__ez_bridge_activity') }}
+union all 
+select distinct 
+    blockchain, 
+    platform,
+    contract_address,
+    'dex' as contract_type
+from 
+    {{ ref('defi__ez_dex_swaps') }}
+union all
+select distinct 
+    blockchain,
+    protocol,
+    contract_address,
+    'lending' as contract_type
+from 
+    {{ ref('defi__ez_lending_deposits') }}
+union all 
+select distinct 
+    blockchain,
+    protocol,
+    contract_address,
+    'lending' as contract_type
+from 
+    {{ ref('defi__ez_lending_borrows') }}
+)
+select 
+    blockchain,
+    platform,
+    contract_address,
+    contract_type
+from base
+
+qualify row_number() over (partition by blockchain, contract_address order by platform asc, contract_type asc) = 1
